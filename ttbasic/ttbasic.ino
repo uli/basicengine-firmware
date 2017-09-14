d98030d71ad271d12f0e4aca82155541f83470db
Ulrich Hecht <ulrich.hecht@gmail.com>
2017-09-14 12:07:32 +0200

    shut the WDTs up
    Two varieties: Standard SDK requires a timer handler that feeds the dogs,
    because the OS will turn the software WDT back on as soon as it gets the
    opportunity.
    
    For SDKNoWifi, it's sufficient to turn off the hardware WDT.

--- ttbasic/ttbasic.ino
+++ ttbasic/ttbasic.ino
@@ -8,6 +8,30 @@
 // 2017/03/22 修正, Arduino STM32、フルスクリーン対応 v0.1 by たま吉さん
 // 
 
+extern "C" {
+#ifdef ESP8266_NOWIFI
+#include <hw/pin_mux_register.h>
+#else
+#include <eagle_soc.h>
+#endif
+};
+#ifndef ESP8266_NOWIFI
+extern "C" {
+#include <user_interface.h>
+};
+
+void ICACHE_RAM_ATTR shut_up_dog(void)
+{
+  // Any attempt to disable the software watchdog is subverted by the SDK
+  // by re-enabling it as soon as it gets the chance. This is the only
+  // way to avoid watchdog resets if you actually want to do anything
+  // with your system that is not wireless bullshit.
+  system_soft_wdt_feed();
+  // See you in one second:
+  timer0_write(ESP.getCycleCount() + F_CPU);
+}
+#endif
+
 void basic(void);
 
 void setup(void){
@@ -25,9 +49,16 @@ void setup(void){
   //delay(3000);
 #endif
   //randomSeed(analogRead(PA0));
+#ifndef ESP8266_NOWIFI
+  timer0_isr_init();
+  timer0_attachInterrupt(&shut_up_dog);
+#endif
 }
 
 void loop(void){
+#ifdef ESP8266_NOWIFI
+  ets_wdt_disable();
+#endif
   // put your main code here, to run repeatedly:
   basic();
 }
