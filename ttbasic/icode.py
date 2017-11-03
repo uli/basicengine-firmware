cc9b0fa3a739999f5e04f75107cbebc9722dd890
Ulrich Hecht <ulrich.hecht@gmail.com>
2017-11-03 13:15:16 +0100

    basic: move keyword table to flash

--- ttbasic/icode.py
+++ ttbasic/icode.py
@@ -3,35 +3,50 @@ f = open('icode.txt', 'r')
 cmdf = open('kwtbl.h', 'w')
 enumf = open('kwenum.h', 'w')
 funf = open('funtbl.h', 'w')
-cmdf.write('static const char * const kwtbl[] __FLASH__ = {\n')
 enumf.write('enum {\n')
 funf.write('static const cmd_t funtbl[] __FLASH__ = {\n')
 count = 0
 max_kw_len = 0
+last_cmd = 0
 for l in f.readlines():
   if len(l) == 0 or l.startswith('rem'):
     continue
   if count % 8 == 0:
-    cmdf.write('  ')
     enumf.write('  ')
     funf.write('  ')
   cmd, enum, fun = l.split()
   if cmd != '_none':
-    cmdf.write('"' + cmd + '",')
+    if last_cmd > 0:
+      print 'error: _none commands must be last'
+      exit(1)
+    cmdf.write('static const char __cmd' + str(count) + ' [] PROGMEM = "' + cmd + '";\n')
     if len(cmd) > max_kw_len:
       max_kw_len = len(cmd)
+  else:
+    if last_cmd == 0:
+      last_cmd = count
+    
   enumf.write(enum + ',')
   funf.write(fun + ',')
   if count % 8 == 7:
-    cmdf.write('\n')
     enumf.write('\n')
     funf.write('\n')
   else:
-    cmdf.write(' ')
     enumf.write(' ')
     funf.write(' ')
   count += 1
-cmdf.write('\n};\n')
+
+if last_cmd == 0:
+  last_cmd = count
+
+cmdf.write('\nstatic const char * const kwtbl[] PROGMEM = {\n')
+for i in range(0, last_cmd):
+  if i % 8 == 0:
+    cmdf.write('  ')
+  cmdf.write('__cmd' + str(i) + ', ')
+  if i % 8 == 7:
+    cmdf.write('\n')
+  
 cmdf.write('\n};\n\n#define MAX_KW_LEN ' + str(max_kw_len) + '\n')
 enumf.write('\n};\n')
 funf.write('\n};\n')
