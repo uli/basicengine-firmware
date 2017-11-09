3a6bd5026443a2e6f394408b5704a571d11862fa
Ulrich Hecht <ulrich.hecht@gmail.com>
2017-11-09 16:22:08 +0100

    tscreenBase::putch(): do not double-type every character
    movePosNextNewChar() calls MOVE(), which redraws the character under the
    cursor before moving it. If the cursor is enabled it is therefore not
    necessary to draw the character here.

--- libraries/TTBAS_LIB/tscreenBase.cpp
+++ libraries/TTBAS_LIB/tscreenBase.cpp
@@ -147,7 +147,8 @@ void tscreenBase::delete_char() {
 // 文字の出力
 void tscreenBase::putch(uint8_t c) {
  VPOKE(pos_x, pos_y, c);
- WRITE(pos_x, pos_y, c);
+ if (!flgCur)
+   WRITE(pos_x, pos_y, c);
  movePosNextNewChar();
 }
 
