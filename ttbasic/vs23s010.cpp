df2d08cf17be9eac154b1610db3f2a5af50564d0
Ulrich Hecht <ulrich.hecht@gmail.com>
2017-11-17 15:06:25 +0100

    vs23: updateBg(): fix calculation of vertical end tile for uneven heights

--- ttbasic/vs23s010.cpp
+++ ttbasic/vs23s010.cpp
@@ -557,15 +557,16 @@ void ICACHE_RAM_ATTR VS23S010::updateBg()
 
     SPI1CLK = absolute_min_spi_div;
 
-    int tile_start_y = bg->scroll_y / bg->tile_size_y;
-    int tile_end_y = tile_start_y + (bg->win_h + bg->tile_size_y-1) / bg->tile_size_y + 1;
-    int tile_start_x = bg->scroll_x / bg->tile_size_x;
-    int tile_end_x = tile_start_x + (bg->win_w + bg->tile_size_x-1) / bg->tile_size_x + 1;
-
     uint32_t tsx = bg->tile_size_x;
     uint32_t tsy = bg->tile_size_y;
     uint32_t xpoff = bg->scroll_x % tsx;
     uint32_t ypoff = bg->scroll_y % tsy;
+
+    int tile_start_y = bg->scroll_y / tsy;
+    int tile_end_y = tile_start_y + (bg->win_h + ypoff) / bg->tile_size_y + 1;
+    int tile_start_x = bg->scroll_x / bg->tile_size_x;
+    int tile_end_x = tile_start_x + (bg->win_w + bg->tile_size_x-1) / bg->tile_size_x + 1;
+
     uint32_t pw = bg->pat_w;
     pat_start_addr = pixelAddr(bg->pat_x, bg->pat_y);
     win_start_addr = pixelAddr(bg->win_x, bg->win_y);
