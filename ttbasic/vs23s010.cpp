eb388e3b743e8af0245a8854917f87cd99d43e0a
Ulrich Hecht <ulrich.hecht@gmail.com>
2018-02-22 17:14:27 +0100

    vs23: reset(): set color space to a defined value
    The fixed conversion does not work for color space 1.

--- ttbasic/vs23s010.cpp
+++ ttbasic/vs23s010.cpp
@@ -100,7 +100,7 @@ void VS23S010::reset()
   resetSprites();
   resetBgs();
   m_bin.Init(m_current_mode->x, m_current_mode->y);
-  setColorConversion(0, 7, 3, 6, true);
+  setColorSpace(0);
 }
 
 void SMALL VS23S010::setMode(uint8_t mode)
