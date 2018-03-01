989f882e5b47b23e50487e620fcd8a17d68d1909
Ulrich Hecht <ulrich.hecht@gmail.com>
2018-03-01 14:01:18 +0100

    update: add user-area flashing (untested)
    Allows flashing to whatever is left between EB and SPIFFS.

--- ttbasic/update.cpp
+++ ttbasic/update.cpp
@@ -5,8 +5,102 @@
 #include "vs23s010.h"
 #include <sdfiles.h>
 #include <Updater.h>
+#include <spi_flash.h>
 
 extern "C" void startup(void);
+
+#define PRINT_P(num, msg) \
+  static const char _msg_ ## num[] PROGMEM = \
+    msg; \
+  c_puts_P(_msg_ ## num)
+#define PRINTLN_P(num, msg) PRINT_P(num, msg); newline()
+
+static void flash_user(BString filename, int sector)
+{
+  uint8_t *buf;
+  int x, y, count;
+
+  // Check that there is at least 32k of memory available,
+  // just to be on the safe side.
+  if (umm_free_heap_size() < 32768 ||
+      !(buf = (uint8_t *)malloc(SPI_FLASH_SEC_SIZE))) {
+    err = ERR_OOM;
+    return;
+  }
+  Unifile f = Unifile::open(filename, FILE_READ);
+  if (!f) {
+    err = ERR_FILE_OPEN;
+    return;
+  }
+
+  uint32_t size = f.fileSize();
+  int end_sector = sector + (size + SPI_FLASH_SEC_SIZE - 1) / SPI_FLASH_SEC_SIZE;
+
+  newline();
+  PRINTLN_P(us0a, "Are you sure you want to overwrite the program");
+  PRINT_P(us1, "currently installed in sectors ");
+  putnum(sector, 0); PRINT_P(to, " to "); putnum(end_sector - 1, 0);
+  PRINT_P(us2, " with ");
+  c_puts(filename.c_str());
+  c_putch('?'); newline(); newline();
+
+  PRINT_P(us8, "Enter YES to continue, anything else to abort: ");
+  get_input();
+  if (strcmp(lbuf, "YES")) {
+    PRINTLN_P(usabrt, "Aborting.");
+    goto out;
+  }
+
+  sc0.show_curs(0);
+  PRINT_P(es, "Erasing sector ");
+  x = sc0.c_x(); y = sc0.c_y();
+  for (int i = sector; i < end_sector; ++i) {
+    sc0.locate(x, y);
+    putnum(i, 0);
+    noInterrupts();
+    if (!ESP.flashEraseSector(i)) {
+      interrupts();
+      newline();
+      err = ERR_IO;
+      return;
+    }
+    interrupts();
+  }
+
+  newline();
+  PRINT_P(wd, "Writing data: ");
+  x = sc0.c_x(); y = sc0.c_y();
+  sc0.locate(x + 7, y);
+  c_putch('/'); putnum(f.fileSize(), 6); PRINT_P(usb, " bytes");
+
+  count = 0;
+  for (;;) {
+    sc0.locate(x, y);
+    putnum(count, 6);
+
+    ssize_t redd = f.read((char *)buf, SPI_FLASH_SEC_SIZE);
+    if (redd < 0) {
+      err = ERR_IO;
+      goto out;
+    }
+    if (!redd)
+      break;
+    noInterrupts();
+    if (!ESP.flashWrite(sector * SPI_FLASH_SEC_SIZE + count, (uint32_t *)buf, redd)) {
+      interrupts();
+      err = ERR_IO;
+      goto out;
+    }
+    interrupts();
+    count += redd;
+  }
+  newline();
+
+out:
+  free(buf);
+  f.close();
+  sc0.show_curs(1);
+}
 
 void iflash()
 {
@@ -30,17 +124,21 @@ void iflash()
   if (!(filename = getParamFname()))
     return;
 
+  int32_t sector = 0;
+  if (*cip == I_COMMA) {
+    ++cip;
+    getParam(sector, 524288 / SPI_FLASH_SEC_SIZE, 1048576 / SPI_FLASH_SEC_SIZE - 1, I_NONE);
+    if (err)
+      return;
+    flash_user(filename, sector);
+    return;
+  }
+
   Unifile f = Unifile::open(filename, FILE_READ);
   if (!f) {
     err = ERR_FILE_OPEN;
     return;
   }
-
-#define PRINT_P(num, msg) \
-  static const char _msg_ ## num[] PROGMEM = \
-    msg; \
-  c_puts_P(_msg_ ## num)
-#define PRINTLN_P(num, msg) PRINT_P(num, msg); newline()
 
   newline();
   sc0.setColor(0, col_warn);
