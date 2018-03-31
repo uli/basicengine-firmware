373710504fd9620fd9789c429872420e6095f5e6
Ulrich Hecht <ulrich.hecht@gmail.com>
2018-03-31 19:31:07 +0200

    icode.py: create string function table with handlers starting with "s"

--- ttbasic/icode.py
+++ ttbasic/icode.py
@@ -3,12 +3,15 @@ f = open('icode.txt', 'r')
 cmdf = open('kwtbl.h', 'w')
 enumf = open('kwenum.h', 'w')
 funf = open('funtbl.h', 'w')
+strfunf = open('strfuntbl.h', 'w')
 enumf.write('#ifndef __KWENUM_H\n#define __KWENUM_H\nenum token_t {\n')
 funf.write('static const cmd_t funtbl[] GROUP(basic_data) = {\n')
 count = 0
 max_kw_len = 0
 last_cmd = 0
 nulls = []
+strfuns = []
+strfun_count = 0
 for l in f.readlines():
   if len(l) == 0 or l.startswith('rem'):
     continue
@@ -25,7 +28,7 @@ for l in f.readlines():
     max_kw_len = len(cmd)
     
   enumf.write(enum + ',')
-  if fun != 'esyntax' and last_cmd > 0:
+  if fun != 'esyntax' and last_cmd > 0 and fun[0] != 's':
     print 'esyntax tokens must be last'
     exit(1)
   if fun == 'esyntax' and last_cmd == 0:
@@ -40,6 +43,12 @@ for l in f.readlines():
     enumf.write(' ')
     if last_cmd == 0:
       funf.write(' ')
+  if fun[0] == 's':
+    if strfun_count == 0:
+      strfun_first = count
+    strfuns += [(enum, fun)]
+    strfun_count += 1
+    
   count += 1
 
 cmdf.write('\nstatic const char * const kwtbl[] PROGMEM = {\n')
@@ -56,3 +65,14 @@ for i in range(0, count):
 cmdf.write('\n};\n\n#define MAX_KW_LEN ' + str(max_kw_len) + '\n')
 enumf.write('\n};\n#endif\n')
 funf.write('\n};\n')
+
+strfunf.write('\nstatic const strfun_t strfuntbl[] GROUP(basic_data) = {\n')
+for i in range(0, strfun_count):
+  if i % 8 == 0:
+    strfunf.write(' ')
+  strfunf.write(' ' + strfuns[i][1] + ',')
+  if i % 8 == 7:
+    strfunf.write('\n')
+strfunf.write('\n};\n')
+strfunf.write('#define STRFUN_FIRST ' + str(strfun_first) + '\n')
+strfunf.write('#define STRFUN_LAST ' + str(strfun_first + strfun_count) + '\n')
