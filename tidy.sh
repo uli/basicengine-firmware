ae55a96615509656a002936ce5e10ae7ed3d812c
Ulrich Hecht <ulrich.hecht@gmail.com>
2018-05-28 16:21:54 +0200

    clean up tidy.sh
    - use what is used to build, not random stuff lying around somewhere
    - add ESP8266SAM, Time libs

--- tidy.sh
+++ tidy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-ARDUINO_DIR=$HOME/.arduino15
+ARDUINO_DIR=../../arduino-1.8.3
 ARDUINO_BSP=../../Arduino_nowifi
-TOOLCHAIN_DIR=${ARDUINO_DIR}/packages/esp8266/tools/xtensa-lx106-elf-gcc/1.20.0-26-gb404fb9-2
-SDFAT_DIR=../../../arduino/SdFat
+TOOLCHAIN_DIR=${ARDUINO_BSP}/tools/xtensa-lx106-elf
 LIBS_DIR=../libraries
-
 clang-tidy-5.0 "$@" -- \
+  -I${ARDUINO_DIR}/libraries/Time \
   -I${TOOLCHAIN_DIR}/xtensa-lx106-elf/include/c++/4.8.2/xtensa-lx106-elf \
   -I${TOOLCHAIN_DIR}/xtensa-lx106-elf/include/c++/4.8.2 \
   -I${TOOLCHAIN_DIR}/lib/gcc/xtensa-lx106-elf/4.8.2/include \
@@ -13,6 +12,7 @@ clang-tidy-5.0 "$@" -- \
   -I${ARDUINO_BSP}/tools/sdk/include \
   -I${ARDUINO_BSP}/variants/basic_engine \
   -I${LIBS_DIR}/TTVoutfonts -I${ARDUINO_BSP}/libraries/SPI -I${LIBS_DIR}/TTBAS_LIB -I${LIBS_DIR}/TKeyboard/src \
-  -I${ARDUINO_BSP}/libraries/Wire -I${SDFAT_DIR}/src \
+  -I${LIBS_DIR}/ESP8266SAM/src \
+  -I${ARDUINO_BSP}/libraries/Wire -I${LIBS_DIR}/SdFat/src \
   -D_GCC_LIMITS_H_ -DARDUINO -D__XTENSA__ -DESP8266 -DESP8266_NOWIFI -D__ets__ \
   -std=c++11 -m32 -U__GNUC__ -Wno-unknown-attributes -Wall
