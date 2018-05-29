a2ed7d516d6e444504049c8019477d7ac15699ed
Ulrich Hecht <ulrich.hecht@gmail.com>
2018-05-29 15:30:03 +0200

    mkref.sh: import some C++ macros into the AsciiDoc template

--- ttbasic/scripts/mkref.sh
+++ ttbasic/scripts/mkref.sh
@@ -1,4 +1,18 @@
-rm -f ../doc/reference.adoc
+function import_def {
+	val="`grep -h "#define\s*${1}\s*" *.h |sed "s,#define\s*${1}\s*,,g"`"
+	echo ":$1: ${val}" >>$REFDOC
+	echo ":$1_m1: $((val-1))" >>$REFDOC
+}
+
+REFDOC=../doc/reference.adoc
+rm -f $REFDOC
+
+import_def VS23_MAX_SPRITES
+import_def VS23_MAX_BG
+import_def VS23_MAX_SPRITE_W
+import_def VS23_MAX_SPRITE_H
+import_def VS23_NUM_COLORSPACES
+
 cat scripts/groups.txt|while read grp desc; do
   sed -nf scripts/bdoc_1.sed <basic.cpp|sed -zf scripts/bdoc_1a.sed|
   	  grep "/\*\*\*b[a-z]\s*${grp}\s*"|
