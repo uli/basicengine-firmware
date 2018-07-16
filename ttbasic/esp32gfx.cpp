eece7148d02ef63a5285d6c11d230e7618ad0af3
Ulrich Hecht <ulrich.hecht@gmail.com>
2018-07-16 17:55:16 +0200

    esp32gfx: add BG engine stub
    Everything except the actual drawing routine, basically.
    
    XXX: Massive copy/paste from VS23 driver, might want to abstract that.

--- ttbasic/esp32gfx.cpp
+++ ttbasic/esp32gfx.cpp
@@ -4,6 +4,7 @@
 #include <soc/rtc.h>
 #include "esp32gfx.h"
 #include "colorspace.h"
+#include "Psx.h"
 
 ESP32GFX vs23;
 
@@ -40,6 +41,11 @@ void ESP32GFX::begin(bool interlace, bool lowpass, uint8_t system)
 
 void ESP32GFX::reset()
 {
+#ifdef USE_BG_ENGINE
+  m_bg_modified = true;
+  resetSprites();
+  resetBgs();
+#endif
   for (int i = 0; i < LAST_LINE; ++i)
     memset(m_pixels[i], 0, XRES);
   m_bin.Init(m_current_mode.x, LAST_LINE - m_current_mode.y);
@@ -86,4 +92,364 @@ void ESP32GFX::setMode(uint8_t mode)
 {
 }
 
+void ESP32GFX::updateBg()
+{
+}
+
+#ifdef USE_BG_ENGINE
+bool ESP32GFX::setBgSize(uint8_t bg_idx, uint16_t width, uint16_t height)
+{
+  struct bg_t *bg = &m_bg[bg_idx];
+
+  m_bg_modified = true;
+  bg->enabled = false;
+
+  if (bg->tiles)
+    free(bg->tiles);
+  bg->tiles = (uint8_t *)calloc(width * height, 1);
+  if (!bg->tiles)
+    return true;
+
+  bg->w = width;
+  bg->h = height;
+  bg->scroll_x = bg->scroll_y = 0;
+  bg->win_x = bg->win_y = 0;
+  bg->win_w = m_current_mode.x;
+  bg->win_h = m_current_mode.y;
+  return false;
+}
+
+void ESP32GFX::enableBg(uint8_t bg)
+{
+  m_bg_modified = true;
+  if (m_bg[bg].tiles) {
+    m_bg[bg].enabled = true;
+  }
+}
+
+void ESP32GFX::disableBg(uint8_t bg)
+{
+  m_bg_modified = true;
+  m_bg[bg].enabled = false;
+}
+
+void ESP32GFX::freeBg(uint8_t bg_idx)
+{
+  struct bg_t *bg = &m_bg[bg_idx];
+  m_bg_modified = true;
+  bg->enabled = false;
+  if (bg->tiles) {
+    free(bg->tiles);
+    bg->tiles = NULL;
+  }
+  if (bg->tile_map) {
+    free(bg->tile_map);
+    bg->tile_map = NULL;
+  }
+}
+
+void ESP32GFX::setBgWin(uint8_t bg_idx, uint16_t x, uint16_t y, uint16_t w, uint16_t h)
+{
+  struct bg_t *bg = &m_bg[bg_idx];
+  bg->win_x = x;
+  bg->win_y = y;
+  bg->win_w = w;
+  bg->win_h = h;
+  m_bg_modified = true;
+}
+
+void ESP32GFX::resetSprites()
+{
+  m_bg_modified = true;
+  for (int i = 0; i < MAX_SPRITES; ++i) {
+    struct sprite_t *s = &m_sprite[i];
+    m_sprites_ordered[i] = s;
+    s->enabled = false;
+    s->pos_x = s->pos_y = 0;
+    s->p.frame_x = s->p.frame_y = 0;
+    s->p.w = s->p.h = 8;
+    s->p.key = 0;
+    s->prio = MAX_PRIO;
+    s->p.flip_x = s->p.flip_y = false;
+    s->p.opaque = false;
+  }
+}
+
+void ESP32GFX::resetBgs()
+{
+  m_bg_modified = true;
+  for (int i=0; i < MAX_BG; ++i) {
+    struct bg_t *bg = &m_bg[i];
+    freeBg(i);
+    bg->tile_size_x = 8;
+    bg->tile_size_y = 8;
+    bg->pat_x = 0;
+    bg->pat_y = m_current_mode.y + 8;
+    bg->pat_w = m_current_mode.x / bg->tile_size_x;
+    bg->prio = i;
+  }
+}
+
+void ESP32GFX::resizeSprite(uint8_t num, uint8_t w, uint8_t h)
+{
+  struct sprite_t *s = &m_sprite[num];
+  if (w != s->p.w || h != s->p.h) {
+    s->p.w = w;
+    s->p.h = h;
+    m_bg_modified = true;
+  }
+}
+
+void GROUP(basic_esp32gfx) ESP32GFX::setSpriteFrame(uint8_t num, uint8_t frame_x, uint8_t frame_y, bool flip_x, bool flip_y)
+{
+  struct sprite_t *s = &m_sprite[num];
+  if (frame_x != s->p.frame_x || frame_y != s->p.frame_y ||
+      flip_x != s->p.flip_x || flip_y != s->p.flip_y) {
+    s->p.frame_x = frame_x;
+    s->p.frame_y = frame_y;
+    s->p.flip_x = flip_x;
+    s->p.flip_y = flip_y;
+    // XXX: opaque?
+    m_bg_modified = true;
+  }
+}
+
+void ESP32GFX::setSpriteKey(uint8_t num, int16_t key)
+{
+  struct sprite_t *s = &m_sprite[num];
+  if (s->p.key != key) {
+    s->p.key = key;
+    m_bg_modified = true;
+  }
+}
+
+void GROUP(basic_esp32gfx) ESP32GFX::setSpritePattern(uint8_t num, uint16_t pat_x, uint16_t pat_y)
+{
+  struct sprite_t *s = &m_sprite[num];
+  if (s->p.pat_x != pat_x || s->p.pat_y != pat_y) {
+    s->p.pat_x = pat_x;
+    s->p.pat_y = pat_y;
+    s->p.frame_x = s->p.frame_y = 0;
+
+    m_bg_modified = true;
+  }
+}
+
+void ESP32GFX::enableSprite(uint8_t num)
+{
+  struct sprite_t *s = &m_sprite[num];
+  if (!s->enabled) {
+    s->enabled = true;
+    m_bg_modified = true;
+  }
+}
+
+void ESP32GFX::disableSprite(uint8_t num)
+{
+  struct sprite_t *s = &m_sprite[num];
+  if (s->enabled) {
+    s->enabled = false;
+    m_bg_modified = true;
+  }
+}
+
+int GROUP(basic_esp32gfx) ESP32GFX::cmp_sprite_y(const void *one, const void *two)
+{
+  return (*(struct sprite_t**)one)->pos_y - (*(struct sprite_t **)two)->pos_y;
+}
+
+void GROUP(basic_esp32gfx) ESP32GFX::moveSprite(uint8_t num, int16_t x, int16_t y)
+{
+  sprite_t *s = &m_sprite[num];
+  if (s->pos_x != x || s->pos_y != y) {
+    s->pos_x = x;
+    s->pos_y = y;
+    qsort(m_sprites_ordered, MAX_SPRITES, sizeof(struct sprite_t *), cmp_sprite_y);
+    m_bg_modified = true;
+  }
+}
+
+void ESP32GFX::mapBgTile(uint8_t bg_idx, uint8_t from, uint8_t to)
+{
+  struct bg_t *bg = &m_bg[bg_idx];
+  if (!bg->tile_map) {
+    bg->tile_map = (uint8_t *)malloc(256);
+    for (int i = 0; i < 256; ++i)
+      bg->tile_map[i] = i;
+  }
+  bg->tile_map[from] = to;
+}
+
+void ESP32GFX::setBgTile(uint8_t bg_idx, uint16_t x, uint16_t y, uint8_t t)
+{
+  struct bg_t *bg = &m_bg[bg_idx];
+
+  if (bg->tile_map)
+    t = bg->tile_map[t];
+
+  int toff = (y % bg->h) * bg->w + (x % bg->w);
+  if (bg->tiles[toff] == t)
+    return;
+  else
+    bg->tiles[toff] = t;
+
+  m_bg_modified = true;
+}
+
+void ESP32GFX::setBgTiles(uint8_t bg_idx, uint16_t x, uint16_t y, const uint8_t *tiles, int count)
+{
+  struct bg_t *bg = &m_bg[bg_idx];
+
+  int off = (y % bg->h) * bg->w;
+  for (int xx = x; xx < x+count; ++xx) {
+    uint8_t t = *tiles++;
+    if (bg->tile_map)
+      t = bg->tile_map[t];
+    bg->tiles[off + (xx % bg->w)] = t;
+  }
+  m_bg_modified = true;
+}
+
+void ESP32GFX::spriteTileCollision(uint8_t sprite, uint8_t bg_idx, uint8_t *tiles, uint8_t num_tiles)
+{
+  sprite_t *spr = &m_sprite[sprite];
+  bg_t *bg = &m_bg[bg_idx];
+  int tsx = bg->tile_size_x;
+  int tsy = bg->tile_size_y;
+  uint8_t res[num_tiles];
+  
+  // Intialize results with "no collision".
+  memset(res, 0, num_tiles);
+  
+  // Check if sprite is overlapping with background at all.
+  if (spr->pos_x + spr->p.w <= bg->win_x ||
+      spr->pos_x >= bg->win_x + bg->win_w ||
+      spr->pos_y + spr->p.h <= bg->win_y ||
+      spr->pos_y >= bg->win_y + bg->win_h)
+    return;
+  
+  // Calculate pixel coordinates of top/left sprite corner in relation to
+  // the background's origin.
+  int bg_left_x = bg->scroll_x - bg->win_x + spr->pos_x;
+  int bg_top_y = bg->scroll_y - bg->win_y + spr->pos_y;
+
+  // Calculate offset, width and height in tiles.
+  // round down top/left
+  int bg_first_tile_off = bg_left_x / tsx + bg->w * (bg_top_y / tsy);
+  // round up width/height
+  int bg_tile_width = (spr->p.w + tsx - 1) / tsx;
+  int bg_tile_height =  (spr->p.h + tsy - 1) / tsy;
+  
+  int bg_last_tile_off = bg_first_tile_off + bg_tile_width + bg_tile_height * bg->w;
+  
+  // Iterate over all tiles overlapping the sprite and record in what way they are
+  // overlapping.
+  // For every line
+  for (int t = bg_first_tile_off, ty = bg_top_y; t < bg_last_tile_off; t += bg->w, ty += tsy) {
+    // For every column
+    for (int tt = t, tx = bg_left_x; tt < t + bg->w; ++tt, tx += tsx) {
+      // For every tile code to be checked
+      for (int m = 0; m < num_tiles; ++m) {
+        if (tiles[m] == bg->tiles[tt % (bg->w*bg->h)]) {
+          res[m] = 0x40;	// indicates collision in general
+          if (tx < spr->pos_x)
+            res[m] |= psxLeft;
+          else if (tx + tsx > spr->pos_x + spr->p.w)
+            res[m] |= psxRight;
+          if (ty < spr->pos_y)
+            res[m] |= psxUp;
+          else if (ty + tsy > spr->pos_y + spr->p.h)
+            res[m] |= psxDown;
+        }
+      }
+    }
+  }
+  os_memcpy(tiles, res, num_tiles);
+}
+
+uint8_t ESP32GFX::spriteTileCollision(uint8_t sprite, uint8_t bg, uint8_t tile)
+{
+  spriteTileCollision(sprite, bg, &tile, 1);
+  return tile;
+}
+
+uint8_t GROUP(basic_esp32gfx) ESP32GFX::spriteCollision(uint8_t collidee, uint8_t collider)
+{
+  uint8_t dir = 0x40;	// indicates collision
+
+  sprite_t *us = &m_sprite[collidee];
+  sprite_t *them = &m_sprite[collider];
+  
+  if (us->pos_x + us->p.w < them->pos_x)
+    return 0;
+  if (them->pos_x + them->p.w < us->pos_x)
+    return 0;
+  if (us->pos_y + us->p.h < them->pos_y)
+    return 0;
+  if (them->pos_y + them->p.h < us->pos_y)
+    return 0;
+  
+  // sprite frame as bounding box; we may want something more flexible...
+  if (them->pos_x < us->pos_x)
+    dir |= psxLeft;
+  else if (them->pos_x + them->p.w > us->pos_x + us->p.w)
+    dir |= psxRight;
+
+  sprite_t *upper = us, *lower = them;
+  if (them->pos_y < us->pos_y) {
+    dir |= psxUp;
+    upper = them;
+    lower = us;
+  } else if (them->pos_y + them->p.h > us->pos_y + us->p.h)
+    dir |= psxDown;
+
+#if 0
+  // Check for pixels in overlapping area.
+  bool really = false;
+  for (int y = lower->pos_y - upper->pos_y; y < upper->p.h; ++y) {
+    // Check if both sprites have any pixels in the lines they overlap in.
+    int lower_py = y - lower->pos_y + upper->pos_y;
+    int upper_len, upper_off, lower_len, lower_off;
+    if (upper->pat) {
+      sprite_line *upper_line = &upper->pat->lines[y];
+      upper_len = upper_line->len;
+      upper_off = upper_line->off;
+    } else {
+      upper_len = upper->p.w;
+      upper_off = 0;
+    }
+    if (lower->pat) {      
+      sprite_line *lower_line = &lower->pat->lines[lower_py];
+      lower_len = lower_line->len;
+      lower_off = lower_line->off;
+    } else {
+      lower_len = lower->p.w;
+      lower_off = 0;
+    }
+    if (upper_len && lower_len) {
+      int dist_x = abs(upper->pos_x - lower->pos_x);
+      int left_len, left_off, right_off;
+      if (upper->pos_x < lower->pos_x) {
+        left_len = upper_len;
+        left_off = upper_off;
+        right_off = lower_off;
+      } else {
+        left_len = lower_len;
+        left_off = lower_off;
+        right_off = upper_off;
+      }
+      if (left_off + left_len >= right_off + dist_x) {
+        really = true;
+        break;
+      }
+    }
+  }
+  if (!really)
+    return 0;
 #endif
+
+  return dir;
+}
+#endif	// USE_BG_ENGINE
+
+#endif	// ESP32
--- ttbasic/esp32gfx.h
+++ ttbasic/esp32gfx.h
@@ -1,6 +1,7 @@
 #ifndef _ESP32GFX_H
 #define _ESP32GFX_H
 
+#include "ttconfig.h"
 #include "SimplePALOutput.h"
 #include "GuillotineBinPack.h"
 
@@ -8,6 +9,14 @@
 #define XRES 320
 #define YRES 240
 #define LAST_LINE 480
+
+#define MAX_BG 4
+
+#define MAX_SPRITES 32
+#define MAX_SPRITE_W 32
+#define MAX_SPRITE_H 32
+
+#define MAX_PRIO	(MAX_BG-1)
 
 struct esp32gfx_mode_t {
   uint16_t x;
@@ -33,6 +42,161 @@ public:
   inline int lastLine() { return LAST_LINE; }
 
   bool blockFinished() { return true; }
+
+#ifdef USE_BG_ENGINE
+  bool setBgSize(uint8_t bg, uint16_t width, uint16_t height);
+
+  inline uint8_t bgWidth(uint8_t bg) {
+    return m_bg[bg].w;
+  }
+  inline uint8_t bgHeight(uint8_t bg) {
+    return m_bg[bg].h;
+  }
+
+  inline void setBgTileSize(uint8_t bg_idx, uint8_t tile_size_x, uint8_t tile_size_y) {
+    struct bg_t *bg = &m_bg[bg_idx];
+    bg->tile_size_x = tile_size_x;
+    bg->tile_size_y = tile_size_y;
+    m_bg_modified = true;
+  }
+
+  inline void setBgPattern(uint8_t bg_idx, uint16_t pat_x, uint16_t pat_y, uint16_t pat_w) {
+    struct bg_t *bg = &m_bg[bg_idx];
+    bg->pat_x = pat_x;
+    bg->pat_y = pat_y;
+    bg->pat_w = pat_w;
+    m_bg_modified = true;
+  }
+  
+  inline void setBgPriority(uint8_t bg_idx, uint8_t prio) {
+    m_bg[bg_idx].prio = prio;
+    m_bg_modified = true;
+  }
+
+  inline uint8_t bgTileSizeX(uint8_t bg) {
+    return m_bg[bg].tile_size_x;
+  }
+  inline uint8_t bgTileSizeY(uint8_t bg) {
+    return m_bg[bg].tile_size_y;
+  }
+
+  inline void getBgTileSize(uint8_t bg_idx, uint8_t &tsx, uint8_t &tsy) {
+    struct bg_t *bg = &m_bg[bg_idx];
+    tsx = bg->tile_size_x;
+    tsy = bg->tile_size_y;
+  }
+
+  inline uint8_t *bgTiles(uint8_t bg) {
+    return m_bg[bg].tiles;
+  }
+
+  void resetBgs();
+  void enableBg(uint8_t bg);
+  void disableBg(uint8_t bg);
+  void freeBg(uint8_t bg);
+
+  void setBgWin(uint8_t bg, uint16_t x, uint16_t y, uint16_t w, uint16_t h);
+  inline uint16_t bgWinX(uint8_t bg) {
+    return m_bg[bg].win_x;
+  }
+  inline uint16_t bgWinY(uint8_t bg) {
+    return m_bg[bg].win_y;
+  }
+  inline uint16_t bgWinWidth(uint8_t bg) {
+    return m_bg[bg].win_w;
+  }
+  inline uint16_t bgWinHeight(uint8_t bg) {
+    return m_bg[bg].win_h;
+  }
+  inline bool bgEnabled(uint8_t bg) {
+    return m_bg[bg].enabled;
+  }
+  inline int bgScrollX(uint8_t bg) {
+    return m_bg[bg].scroll_x;
+  }
+  inline int bgScrollY(uint8_t bg) {
+    return m_bg[bg].scroll_y;
+  }
+  
+  void setBgTile(uint8_t bg_idx, uint16_t x, uint16_t y, uint8_t t);
+  void setBgTiles(uint8_t bg_idx, uint16_t x, uint16_t y, const uint8_t *tiles, int count);
+  void mapBgTile(uint8_t bg_idx, uint8_t from, uint8_t to);
+
+  inline void scroll(uint8_t bg_idx, int x, int y) {
+    struct bg_t *bg = &m_bg[bg_idx];
+    while (x < 0)
+      x += bg->tile_size_x * bg->w;
+    while (y < 0)
+      y += bg->tile_size_y * bg->h;
+    if (bg->scroll_x != x || bg->scroll_y != y) {
+      bg->scroll_x = x;
+      bg->scroll_y = y;
+      m_bg_modified = true;
+    }
+  }
+  void updateBg();
+
+  void resetSprites();
+  void setSpritePattern(uint8_t num, uint16_t pat_x, uint16_t pat_y);
+  void resizeSprite(uint8_t num, uint8_t w, uint8_t h);
+  void moveSprite(uint8_t num, int16_t x, int16_t y);
+  void setSpriteFrame(uint8_t num, uint8_t frame_x, uint8_t frame_y = 0, bool flip_x = false, bool flip_y = false);
+  void setSpriteKey(uint8_t num, int16_t key);
+  void enableSprite(uint8_t num);
+  void disableSprite(uint8_t num);
+  inline uint8_t spriteFrameX(uint8_t num) {
+    return m_sprite[num].p.frame_x;
+  }
+  inline uint8_t spriteFrameY(uint8_t num) {
+    return m_sprite[num].p.frame_y;
+  }
+  inline bool spriteFlipX(uint8_t num) {
+    return m_sprite[num].p.flip_x;
+  }
+  inline bool spriteFlipY(uint8_t num) {
+    return m_sprite[num].p.flip_y;
+  }
+  inline bool spriteOpaque(uint8_t num) {
+    return !m_sprite[num].p.opaque;
+  }
+  inline int16_t spriteX(uint8_t num) {
+    return m_sprite[num].pos_x;
+  }
+  inline int16_t spriteY(uint8_t num) {
+    return m_sprite[num].pos_y;
+  }
+
+  inline void setSpriteOpaque(uint8_t num, bool enable) {
+  }
+
+  inline bool spriteEnabled(uint8_t num) {
+    return m_sprite[num].enabled;
+  }
+
+  inline void setSpritePriority(uint8_t num, uint8_t prio) {
+    m_sprite[num].prio = prio;
+    m_bg_modified = true;
+  }
+
+  inline bool spriteReload(uint8_t num) {
+    return true;
+  }
+
+  void spriteTileCollision(uint8_t sprite, uint8_t bg, uint8_t *tiles, uint8_t num_tiles);
+  uint8_t spriteTileCollision(uint8_t sprite, uint8_t bg, uint8_t tile);
+  uint8_t spriteCollision(uint8_t collidee, uint8_t collider);
+
+  inline void forceRedraw() {
+    m_bg_modified = true;
+  }
+  
+  inline void setFrameskip(uint32_t v) {
+    m_frameskip = v;
+  }
+  inline uint32_t getFrameskip() {
+    return m_frameskip;
+  }
+#endif
 
   bool allocBacking(int w, int h, int &x, int &y);
   void freeBacking(int x, int y, int w, int h);
@@ -92,6 +256,41 @@ private:
 
   uint32_t m_frame;
   uint8_t *m_pixels[LAST_LINE];
+
+#ifdef USE_BG_ENGINE
+  struct bg_t {
+    uint8_t *tiles;
+    uint16_t pat_x, pat_y, pat_w;
+    uint8_t w, h;
+    uint8_t tile_size_x, tile_size_y;
+    int scroll_x, scroll_y;
+    uint16_t win_x, win_y, win_w, win_h;
+    bool enabled;
+    uint8_t prio;
+    uint8_t *tile_map;
+  } m_bg[MAX_BG];
+
+  struct sprite_props {
+    uint16_t pat_x, pat_y;
+    uint8_t w, h;
+    uint8_t frame_x, frame_y;
+    uint8_t key;
+    bool flip_x:1, flip_y:1, opaque:1;
+  };
+    
+  struct sprite_t {
+    struct sprite_props p;
+    int16_t pos_x, pos_y;
+    uint8_t prio;
+    bool enabled:1;
+  } m_sprite[MAX_SPRITES];
+  struct sprite_t *m_sprites_ordered[MAX_SPRITES];
+  static int cmp_sprite_y(const void *one, const void *two);
+
+  uint32_t m_frameskip;
+
+  bool m_bg_modified;
+#endif
 
   SimplePALOutput m_pal;
 };
