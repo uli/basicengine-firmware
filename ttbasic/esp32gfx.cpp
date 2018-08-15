6ce538b46a87d1cf5beeac42b50d963878f967b6
Ulrich Hecht <uli@basicengine.org>
2018-08-15 17:49:20 +0200

    esp32gfx: setMode(): allocate enough off-screen lines for color memory
    Fixes crashes in high-res modes 10 and 13.

--- ttbasic/esp32gfx.cpp
+++ ttbasic/esp32gfx.cpp
@@ -108,7 +108,11 @@ void ESP32GFX::setMode(uint8_t mode)
   m_current_mode = modes_pal[mode];
   m_pal.setMode(m_current_mode);
 
-  m_last_line = _max(131072 / m_current_mode.x, m_current_mode.y + 8);
+  // Try to allocate no more than 128k, but make sure it's enough to hold
+  // the specified resolution plus color memory.
+  m_last_line = _max(131072 / m_current_mode.x,
+                     m_current_mode.y + m_current_mode.y / MIN_FONT_SIZE_Y);
+
   m_pixels = (uint8_t **)pvPortMallocCaps(sizeof(*m_pixels) * m_last_line, MALLOC_CAP_32BIT);
   for (int i = 0; i < m_last_line; ++i) {
     m_pixels[i] = (uint8_t *)calloc(1, m_current_mode.x);
