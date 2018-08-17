e154b1a054076736d585ddf5645829db92a125b5
Ulrich Hecht <uli@basicengine.org>
2018-08-17 15:47:04 +0200

    scripts: installpackages.sh: add ESP32 dependencies

--- ttbasic/scripts/installpackages.sh
+++ ttbasic/scripts/installpackages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-PACKAGES=asciidoctor
+PACKAGES="asciidoctor libc6-dev libncurses5-dev gperf python-serial exuberant-ctags"
 
 for p in ${PACKAGES} ; do
   if ! dpkg -s "$p" >/dev/null 2>&1 ; then
-	sudo apt-get install "$p"
+	sudo apt-get -y install "$p"
   fi
 done
