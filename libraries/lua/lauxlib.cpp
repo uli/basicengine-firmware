31ef4a14d877908c490ea7fb2262d4aeaac6c54e
Ulrich Hecht <uli@basicengine.org>
2019-01-12 17:36:01 +0100

    lua: auxlib: use Unifile instead of stdio

--- libraries/lua/lauxlib.cpp
+++ libraries/lua/lauxlib.cpp
@@ -694,8 +694,8 @@ static const char *getF (lua_State *L, void *ud, size_t *size) {
     /* 'fread' can return > 0 *and* set the EOF flag. If next call to
        'getF' called 'fread', it might still wait for user input.
        The next check avoids this problem. */
-    if (feof(lf->f)) return NULL;
-    *size = fread(lf->buff, 1, sizeof(lf->buff), lf->f);  /* read block */
+    if (!lf->f->available()) return NULL;
+    *size = lf->f->read(lf->buff, sizeof(lf->buff)); /* read block */
   }
   return lf->buff;
 }
@@ -715,12 +715,12 @@ static int skipBOM (LoadF *lf) {
   int c;
   lf->n = 0;
   do {
-    c = getc(lf->f);
+    c = lf->f->read();
     if (c == EOF || c != *(const unsigned char *)p++) return c;
     lf->buff[lf->n++] = c;  /* to be read by the parser */
   } while (*p != '\0');
   lf->n = 0;  /* prefix matched; discard it */
-  return getc(lf->f);  /* return next character */
+  return lf->f->read();  /* return next character */
 }
 
 
@@ -735,9 +735,9 @@ static int skipcomment (LoadF *lf, int *cp) {
   int c = *cp = skipBOM(lf);
   if (c == '#') {  /* first line is a comment (Unix exec. file)? */
     do {  /* skip first line */
-      c = getc(lf->f);
+      c = lf->f->read();
     } while (c != EOF && c != '\n');
-    *cp = getc(lf->f);  /* skip end-of-line, if present */
+    *cp = lf->f->read();  /* skip end-of-line, if present */
     return 1;  /* there was a comment */
   }
   else return 0;  /* no comment */
@@ -751,30 +751,46 @@ LUALIB_API int luaL_loadfilex (lua_State *L, const char *filename,
   int c;
   int fnameindex = lua_gettop(L) + 1;  /* index of filename on the stack */
   if (filename == NULL) {
+#if 0
     lua_pushliteral(L, "=stdin");
     lf.f = stdin;
+#else
+    return errfile(L, "stdin", fnameindex);
+#endif
   }
   else {
     lua_pushfstring(L, "@%s", filename);
-    lf.f = fopen(filename, "r");
+    lf.f = new Unifile(); *lf.f = Unifile::open(filename, UFILE_READ);
+    if (!*lf.f) {
+      delete lf.f;
+      lf.f = NULL;
+    }
     if (lf.f == NULL) return errfile(L, "open", fnameindex);
   }
   if (skipcomment(&lf, &c))  /* read initial portion */
     lf.buff[lf.n++] = '\n';  /* add line to correct line numbers */
+#if 0
   if (c == LUA_SIGNATURE[0] && filename) {  /* binary file? */
     lf.f = freopen(filename, "rb", lf.f);  /* reopen in binary mode */
     if (lf.f == NULL) return errfile(L, "reopen", fnameindex);
     skipcomment(&lf, &c);  /* re-read initial portion */
   }
+#endif
   if (c != EOF)
     lf.buff[lf.n++] = c;  /* 'c' is the first character of the stream */
   status = lua_load(L, getF, &lf, lua_tostring(L, -1), mode);
-  readstatus = ferror(lf.f);
-  if (filename) fclose(lf.f);  /* close file (even in case of errors) */
+//  readstatus = ferror(lf.f);
+  if (filename) {
+    lf.f->close();  /* close file (even in case of errors) */
+    delete lf.f;
+    lf.f = NULL;
+  }
+#if 0
   if (readstatus) {
     lua_settop(L, fnameindex);  /* ignore results from 'lua_load' */
     return errfile(L, "read", fnameindex);
   }
+#endif
   lua_remove(L, fnameindex);
   return status;
 }
