d2ddaa1fd494591f1dea02c89a2de263a747117d
Billy J. Beltran <billy@redpill-linpro.com>
2019-02-02 16:01:44 +0100

    Add Fedora support

--- ttbasic/scripts/installpackages_hosted.sh
+++ ttbasic/scripts/installpackages_hosted.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 PACKAGES="g++ libsdl1.2-dev"
 
-for p in ${PACKAGES} ; do
-  if ! dpkg -s "$p" >/dev/null 2>&1 ; then
-	sudo apt-get -y install "$p"
-  fi
-done
+if [ `lsb_release -is` = "Fedora" ]; then
+    PACKAGES="libstdc++-devel SDL-devel"
+    sudo dnf install -y $PACKAGES
+else
+    for p in ${PACKAGES} ; do
+      if ! dpkg -s "$p" >/dev/null 2>&1 ; then
+    	sudo apt-get -y install "$p"
+      fi
+    done
+
+fi
