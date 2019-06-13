4f8e7c27c93d92fac6a031271cf08ceca68ff359
Ulrich Hecht <uli@basicengine.org>
2019-06-13 12:48:20 +0200

    scripts/check8266iram.sh: deal with new defines for other platforms
    Use the first.

--- ttbasic/scripts/check8266iram.sh
+++ ttbasic/scripts/check8266iram.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-bufs=`grep define.*TSF_BUFFS[^I] tsf.h|sed s,^.*BUFFS,,`
-num=`grep define.*TSF_BUFFSIZE tsf.h|sed s,^.*BUFFSIZE,,`
+bufs=`grep define.*TSF_BUFFS[^I] tsf.h|head -n 1|sed s,^.*BUFFS,,`
+num=`grep define.*TSF_BUFFSIZE tsf.h|head -n 1|sed s,^.*BUFFSIZE,,`
 eoi=$(( 16#`objdump -x "$1"|grep _lit4_end|cut -c1-8` ))
 #echo $bufs $num $eoi
 if test $(( 16#40108000 - eoi )) -lt $(( bufs * num * 1 )); then
