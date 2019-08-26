e3fac58eb680626b5ba01d60cfc6977ce931729a
Ulrich Hecht <uli@basicengine.org>
2019-08-26 16:13:52 +0200

    dos: add Arduino glue
    Mostly copied from H3 implementation.

---
+++ dos/Arduino.cpp
@@ -0,0 +1,69 @@
+#include <video_driver.h>
+
+uint32_t Video::frame()
+{
+  static uint32_t frame = 0;
+  return frame++;
+}
+
+#include "Arduino.h"
+#include "FS.h"
+#include "SPI.h"
+#include <malloc.h>
+
+void digitalWrite(uint8_t pin, uint8_t val) {
+  printf("DW %d <- %02X\n", pin, val);
+}
+
+int digitalRead(uint8_t pin) {
+  return 0;
+}
+
+void pinMode(uint8_t pin, uint8_t mode) {
+}
+
+fs::FS SPIFFS;
+SPIClass SPI;
+
+void loop();
+void setup();
+
+struct palette {
+  uint8_t r, g, b;
+};
+#include <N-0C-B62-A63-Y33-N10.h>
+#include <P-EE-A22-B22-Y44-N10.h>
+
+static void my_exit(void)
+{
+  fprintf(stderr, "my_exit\n");
+}
+
+#include <allegro.h>
+
+int main(int argc, char **argv)
+{
+  atexit(my_exit);
+
+  if (allegro_init() != 0)
+    return 1;
+
+  install_keyboard(); 
+  install_timer();
+
+  setup();
+  for (;;)
+    loop();
+}
+
+#include "border_pal.h"
+
+uint64_t total_frames = 0;
+extern uint64_t total_samples;
+extern int sound_reinit_rate;
+
+void platform_pump_events() {
+}
+
+#include "Wire.h"
+TwoWire Wire;
---
+++ dos/Arduino.h
@@ -0,0 +1,135 @@
+#ifndef _ARDUINO_H
+#define _ARDUINO_H
+
+#include <math.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <limits.h>
+#include <time.h>
+
+#include "pgmspace.h"
+#define ICACHE_RAM_ATTR
+#define ICACHE_RODATA_ATTR
+
+#define HIGH 0x1
+#define LOW  0x0
+
+#define INPUT             0x00
+#define INPUT_PULLUP      0x02
+#define OUTPUT            0x01
+#define OUTPUT_OPEN_DRAIN 0x03
+
+#define os_memcpy memcpy
+
+#define noInterrupts()
+#define interrupts()
+
+#define isDigit isdigit
+#define isAlpha isalpha
+#define isAlphaNumeric isalnum
+#define isHexadecimalDigit isxdigit
+
+typedef unsigned int word;
+
+#define bit(b) (1UL << (b))
+#define _BV(b) (1UL << (b))
+
+typedef uint8_t boolean;
+typedef uint8_t byte;
+
+static uint32_t micros() {
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return tv.tv_sec * 1000000 + tv.tv_usec;
+}
+static uint32_t millis() {
+	return micros() / 1000;
+}
+
+#define delay(ms) delayMicroseconds((ms)*1000)
+static void delayMicroseconds(unsigned int us) {
+	uint32_t until = micros() + us;
+	while (micros() < until) {}
+}
+
+#ifdef __cplusplus 
+
+enum SerialConfig {
+  SERIAL_8N1,
+};
+
+class HardwareSerial {
+public:
+  static void begin(unsigned long baud, SerialConfig c=SERIAL_8N1) {
+  }
+  static void println(const char *p) {
+    puts(p); putchar('\n');
+  }
+  static void write(char c) {
+    putchar(c);
+  }
+  static int read() {
+    return -1;
+  }
+  static int available() {
+    return 0;
+  }
+  static void flush() {
+  }
+};
+
+static HardwareSerial Serial;
+
+#include "Esp.h"
+
+#endif // __cplusplus
+
+void digitalWrite(uint8_t pin, uint8_t val);
+int digitalRead(uint8_t pin);
+void pinMode(uint8_t pin, uint8_t mode);
+
+#include <stdlib.h>
+static inline long _random(long v) {
+  return rand() % v;
+}
+#define random _random
+#define randomSeed srand
+
+#ifdef __cplusplus
+// workaround to avoid mix-ups with our min()/max() macros
+#include <memory>
+#endif
+
+#define _min(a,b) ((a)<(b)?(a):(b))
+#define _max(a,b) ((a)>(b)?(a):(b))
+#define min _min
+#define max _max
+
+#include "binary.h"
+
+#include "WString.h"
+
+static inline void yield() {
+}
+
+void platform_pump_events();
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void *
+memmem (const void *haystack_start,
+	size_t haystack_len,
+	const void *needle_start,
+	size_t needle_len);
+void *
+memrchr (const void *src_void,
+	int c,
+	size_t length);
+#ifdef __cplusplus
+};
+#endif
+
+#define TRAC printf("%s:%d\n", __FUNCTION__, __LINE__);
+#endif
