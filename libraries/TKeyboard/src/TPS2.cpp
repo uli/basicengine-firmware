3fb2c2ef9baeb5e92f74564180362fbcb39ce50d
Ulrich Hecht <uli@basicengine.org>
2019-10-09 18:06:44 +0200

    TKeyboard: TPS2.cpp: format code
    Invisible changes omitted. No functional changes.

--- libraries/TKeyboard/src/TPS2.cpp
+++ libraries/TKeyboard/src/TPS2.cpp
@@ -73,8 +73,10 @@ void TPS2::setPort(uint8_t clk, uint8_t dat) { // GPIOピンの設定
 // エラー参照
 // ※割り込みハンドラで発生したエラーチェック用
 // 戻り値  0:正常終了 0以外:異常終了(エラーコード)
-//  
-uint8_t TPS2::error() {return _err; } ;
+//
+uint8_t TPS2::error() {
+  return _err;
+};
 
 //
 // 利用開始
@@ -85,7 +87,7 @@ uint8_t TPS2::error() {return _err; } ;
 // 引数
 //   clk : PS/2 CLK
 //   dat : PS/2 DATA
-// 
+//
 
 void TPS2::begin(uint8_t clk, uint8_t dat) {
   setPort(clk,dat);
@@ -96,7 +98,7 @@ void TPS2::begin(uint8_t clk, uint8_t dat) {
   datSet_Out();
   mode_stop(); // PS/2ライン状態は停止状態
   _err = 0;
-  
+
   // キー入力バッファの初期化
   clear_queue();  
   
@@ -123,31 +125,31 @@ void TPS2::end() {
 #endif
 
 // CLKを出力モードに設定
-void  TPS2::clkSet_Out() {
+void TPS2::clkSet_Out() {
   pinMode(_clkPin, PS2_OUTPUT);
   _clkDir = D_OUT;  
 }
 
 // CLKを入力モードに設定
-void  TPS2::clkSet_In() {
+void TPS2::clkSet_In() {
   pinMode(_clkPin, INPUT_PULLUP);
   _clkDir = D_IN;
 }
 
 // DATを出力モードに設定
-void  TPS2::datSet_Out() {
+void TPS2::datSet_Out() {
   pinMode(_datPin, PS2_OUTPUT);
   _datDir = D_OUT;    
 }
 
 // DATを入力モードに設定
-void  TPS2::datSet_In() {
+void TPS2::datSet_In() {
   pinMode(_datPin, INPUT_PULLUP);
   _datDir = D_IN;
 }
 
 // CLKから出力
-void  TPS2::Clk_Out(uint8_t val) {
+void TPS2::Clk_Out(uint8_t val) {
   if (_clkDir == D_IN)
     clkSet_Out();    
   digitalWrite(_clkPin, val);
@@ -175,7 +177,7 @@ uint8_t ICACHE_RAM_ATTR TPS2::Dat_In() {
 }
 
 // アイドル状態に設定
-void TPS2::mode_idole(uint8_t dir){
+void TPS2::mode_idole(uint8_t dir) {
   if (dir == D_IN) {
     // 入力状態で開放(PINはプルアップ抵抗によりHIGHとなる)
     clkSet_In();
@@ -201,11 +203,11 @@ void TPS2::mode_send() {
 
 // CLKの状態変化待ち
 uint8_t TPS2::wait_Clk(uint8_t val, uint32_t tm) {
-  uint8_t err =1;
-  while(1) {
+  uint8_t err = 1;
+  while (1) {
     if (Clk_In() == val) {
-      err=0;
-      break;          
+      err = 0;
+      break;
     }
     tm--;
     if (!tm)
@@ -217,19 +219,19 @@ uint8_t TPS2::wait_Clk(uint8_t val, uint32_t tm) {
 
 // DATの状態待ち
 uint8_t TPS2::wait_Dat(uint8_t val, uint32_t tm) {
-  uint8_t err =1;
-  while(1) {
+  uint8_t err = 1;
+  while (1) {
     if (Dat_In() == val) {
-      err=0;
-      break;          
+      err = 0;
+      break;
     }
     tm--;
     if (!tm)
       break;
     delayMicroseconds(1);
   }
-  _err =err;
-  return err;  
+  _err = err;
+  return err;
 }
 
 // データ送信
@@ -248,33 +250,69 @@ uint8_t TPS2::hostSend(uint8_t data) {
   for (uint8_t i = 0; i < 8; i++) {
     // 1ビット送信
     delayMicroseconds(15);
-    if ( bitRead(data,i) ) {  parity = !parity; Dat_Out(HIGH); } else { Dat_Out(LOW); }
-    if (wait_Clk(HIGH,50)) { err = 2;  goto ERROR; } // デバイスがクロックをHIGHにするまで待つ
-    if (wait_Clk(LOW, 50)) { err = 3;  goto ERROR; } // デバイスがクロックをLOWにするまで待つ    
+    if (bitRead(data, i)) {
+      parity = !parity;
+      Dat_Out(HIGH);
+    } else {
+      Dat_Out(LOW);
+    }
+    // デバイスがクロックをHIGHにするまで待つ
+    if (wait_Clk(HIGH, 50)) {
+      err = 2;
+      goto ERROR;
+    }
+    // デバイスがクロックをLOWにするまで待つ
+    if (wait_Clk(LOW, 50)) {
+      err = 3;
+      goto ERROR;
+    }
   }
-  
+
   // パリティビットの送信
   delayMicroseconds(15);
-  if (parity) Dat_Out(HIGH); else Dat_Out(LOW);
-  if (wait_Clk(HIGH,50)) { err = 4; goto ERROR; } // デバイスがクロックをHIGHにするまで待つ
-  if (wait_Clk(LOW, 50)){ err = 6; goto ERROR; }  // デバイスがクロックをLOWにするまで待つ    
+  if (parity)
+    Dat_Out(HIGH);
+  else
+    Dat_Out(LOW);
+  // デバイスがクロックをHIGHにするまで待つ
+  if (wait_Clk(HIGH, 50)) {
+    err = 4;
+    goto ERROR;
+  }
+  // デバイスがクロックをLOWにするまで待つ
+  if (wait_Clk(LOW, 50)) {
+    err = 6;
+    goto ERROR;
+  }
 
   // STOPビットの送信
   delayMicroseconds(15);
   Dat_Out(HIGH);
-    
-  // デバイスからのACK確認
-  if (wait_Dat(LOW, 50)){ err = 7; goto ERROR; }
-  if (wait_Clk(LOW, 50)){ err = 8; goto ERROR; }
-  
-  // バス開放待ち
-  if (wait_Dat(HIGH, 50)){ err = 9; goto ERROR; }
-  if (wait_Clk(HIGH, 50)){ err = 10;goto ERROR; }
 
-ERROR: // 終了処理
-  mode_idole(D_IN);        // バスをアイドル状態にする
-  enableInterrupts();       // 割り込み許可
-  return err;  
+  // デバイスからのACK確認
+  if (wait_Dat(LOW, 50)) {
+    err = 7;
+    goto ERROR;
+  }
+  if (wait_Clk(LOW, 50)) {
+    err = 8;
+    goto ERROR;
+  }
+
+  // バス開放待ち
+  if (wait_Dat(HIGH, 50)) {
+    err = 9;
+    goto ERROR;
+  }
+  if (wait_Clk(HIGH, 50)) {
+    err = 10;
+    goto ERROR;
+  }
+
+ERROR:                 // 終了処理
+  mode_idole(D_IN);    // バスをアイドル状態にする
+  enableInterrupts();  // 割り込み許可
+  return err;
 }
 
 //
@@ -282,7 +320,7 @@ ERROR: // 終了処理
 //
 uint8_t TPS2::rcev(uint8_t* rcv) {
   uint8_t err = 1;
-  uint16_t tm=1500;
+  uint16_t tm = 1500;
 
   do {
     if (available()) {
@@ -292,12 +330,12 @@ uint8_t TPS2::rcev(uint8_t* rcv) {
     }
     delayMicroseconds(1);
     tm--;
-  } while(tm);
+  } while (tm);
   return err;
 }
 
 // データ受信
-uint8_t TPS2::HostRcev(uint8_t* rcv) {
+uint8_t TPS2::HostRcev(uint8_t *rcv) {
   uint8_t data = 0;
   bool parity = true;
   uint8_t err = 0;
@@ -306,26 +344,69 @@ uint8_t TPS2::HostRcev(uint8_t* rcv) {
   disableInterrupts();    // 割り込み禁止
 
   // STARTビットの受信
-  if ( wait_Clk(LOW, 1000) ) { err = 1; goto ERROR; }    
-  if ( wait_Dat(LOW, 1) )    { err = 2; goto ERROR; }   // STARTは常に0のはず
-  if ( wait_Clk(HIGH, 50) )  { err = 3; goto ERROR; }      
+  if (wait_Clk(LOW, 1000)) {
+    err = 1;
+    goto ERROR;
+  }
+  // STARTは常に0のはず
+  if (wait_Dat(LOW, 1)) {
+    err = 2;
+    goto ERROR;
+  }
+  if (wait_Clk(HIGH, 50)) {
+    err = 3;
+    goto ERROR;
+  }
 
   //8ビット分データの受信ループ
   for (uint8_t i = 0; i < 8; i++) {
-    if (wait_Clk(LOW, 50)) { err = 4; goto ERROR; }       // CLKがLOWになるのを待つ    
-    if ( Dat_In() ) { parity = !parity; data |= (1<<i); } // 1ビット分の取得
-    if (wait_Clk(HIGH, 50)) { err = 5; goto ERROR; }      // CLKがHIGHになるのを待つ
+    // CLKがLOWになるのを待つ
+    if (wait_Clk(LOW, 50)) {
+      err = 4;
+      goto ERROR;
+    }
+    // 1ビット分の取得
+    if (Dat_In()) {
+      parity = !parity;
+      data |= (1 << i);
+    }
+    // CLKがHIGHになるのを待つ
+    if (wait_Clk(HIGH, 50)) {
+      err = 5;
+      goto ERROR;
+    }
   }
 
   //PARITYビットの取得
-  if (wait_Clk(LOW, 50)){ err = 6; goto ERROR;} // CLKがLOWになるのを待つ
-  if (Dat_In() != parity){ err = 7; goto ERROR; } // パリティチェック
-  if (wait_Clk(HIGH, 50)){ err = 8; goto ERROR; }
+  // CLKがLOWになるのを待つ
+  if (wait_Clk(LOW, 50)) {
+    err = 6;
+    goto ERROR;
+  }
+  // パリティチェック
+  if (Dat_In() != parity) {
+    err = 7;
+    goto ERROR;
+  }
+  if (wait_Clk(HIGH, 50)) {
+    err = 8;
+    goto ERROR;
+  }
 
   // ストップビットの取得
-  if ( wait_Clk(LOW, 50) ) { err = 9; goto ERROR; }    
-  if ( wait_Dat(HIGH, 1) ) { err = 10; goto ERROR; } // STOPは常に1のはず   
-  if ( wait_Clk(HIGH, 50) ) { err = 11; goto ERROR; }      
+  if (wait_Clk(LOW, 50)) {
+    err = 9;
+    goto ERROR;
+  }
+  // STOPは常に1のはず
+  if (wait_Dat(HIGH, 1)) {
+    err = 10;
+    goto ERROR;
+  }
+  if (wait_Clk(HIGH, 50)) {
+    err = 11;
+    goto ERROR;
+  }
 
 ERROR:
 
@@ -336,25 +417,26 @@ ERROR:
 
 // 応答受信
 uint8_t TPS2::response(uint8_t *rcv) {
-  uint8_t tm=250;
-  uint8_t err =0;
-  while(1) {
+  uint8_t tm = 250;
+  uint8_t err = 0;
+  while (1) {
     err = HostRcev(rcv);
-    if (!err) break;          
+    if (!err)
+      break;
     tm--;
-    if (!tm) break;
+    if (!tm)
+      break;
   }
-  return err;  
+  return err;
 }
 
 // 割り込みハンドラ経由データ送信
 uint8_t TPS2::send(uint8_t data) {
-
   // 送信データセット
   _sendData = data;       // 送信データセット
   _flg_rs = 1;
   _sendState = 0;
-  
+
   // 送信準備
   disableInterrupts();    // 割り込み禁止
   mode_stop();            // バスを送信禁止状態に設定
@@ -376,13 +458,12 @@ uint8_t TPS2::send(uint8_t data) {
 // データ送信用
 //
 void ICACHE_RAM_ATTR TPS2::clkPinHandleSend() {
-
   if (Clk_In()) {
-      goto END;
+    goto END;
   }
 
   _sendState++;
-  if ( _sendState >=1 && _sendState <= 8 ) {
+  if (_sendState >= 1 && _sendState <= 8) {
     // データの送信
     delayMicroseconds(15);
     if (_sendData & 1) {
@@ -393,17 +474,17 @@ void ICACHE_RAM_ATTR TPS2::clkPinHandleSend() {
     }
     _sendData >>= 1;
     goto END;
-  } else if ( _sendState == 9 ) {
+  } else if (_sendState == 9) {
     // パリティ送信
     delayMicroseconds(15);
     Dat_Out(_sendParity & 1);
     goto END;
-  } else if ( _sendState == 10 ) {
+  } else if (_sendState == 10) {
     // ストップビット送信
     delayMicroseconds(15);
     Dat_Out(HIGH); 
     goto END;
-  } else if ( _sendState == 11 ) {
+  } else if (_sendState == 11) {
     // ACK確認(確認しない) 終了
     goto DONE;
   } else {
@@ -415,7 +496,7 @@ ERROR:
   _err = _sendState;
 DONE:
   _sendParity = 1;
-  _sendState =0;
+  _sendState = 0;
   _flg_rs = 0;
 END:
   return;
@@ -435,17 +516,17 @@ void ICACHE_RAM_ATTR TPS2::clkPinHandle() {
   }
 
   if (Clk_In()) {
-      goto END;
+    goto END;
   }
-  
+
   state++;
   if (state == 1) {	
     // [0] Stop bit (always LOW)
-    if (Dat_In()) 
-       goto ERROR; 
-  } else if (state >=2 && state <=9 ) {
+    if (Dat_In())
+      goto ERROR;
+  } else if (state >= 2 && state <= 9) {
     // [1-8] Data bits (for 8 bits)
-    data >>=1;
+    data >>= 1;
     if (Dat_In()) {
       data |= 0x80;
       parity++;
@@ -453,13 +534,13 @@ void ICACHE_RAM_ATTR TPS2::clkPinHandle() {
   } else if (state == 10) {
     // Parity bit
     if (Dat_In()) {
-       if (!(parity & 0x01))
-          goto ERROR;
+      if (!(parity & 0x01))
+        goto ERROR;
     } else {
-       if (parity & 0x01)
-          goto ERROR;
+      if (parity & 0x01)
+        goto ERROR;
     }
-  }	else if (state == 11) {
+  } else if (state == 11) {
     // Stop bit
     if (!Dat_In())
       goto ERROR;
@@ -469,11 +550,11 @@ void ICACHE_RAM_ATTR TPS2::clkPinHandle() {
     goto ERROR;	   
   }
   return;
-  
+
 ERROR:
   _err = state;
 DONE:
-  state =0;
+  state = 0;
   data = 0;
   parity = 1;
 END:
@@ -493,9 +574,9 @@ void TPS2::resetState() {
 // キューへの挿入
 void ICACHE_RAM_ATTR TPS2::enqueue(uint8_t data) {
   uint16_t n = (_q_top + 1) % QUEUESIZE;
-  if (_q_top+1 != _q_btm) {
-	_queue[_q_top] = data;
-	_q_top = n;	
+  if (_q_top + 1 != _q_btm) {
+    _queue[_q_top] = data;
+    _q_top = n;
   }
 }
 
@@ -503,15 +584,15 @@ void ICACHE_RAM_ATTR TPS2::enqueue(uint8_t data) {
 uint8_t TPS2::dequeue() {
   uint16_t val = 0;
 
-  if ( _q_top != _q_btm ) {
-     val =_queue[_q_btm];
-     _q_btm = (_q_btm + 1) % QUEUESIZE;
+  if (_q_top != _q_btm) {
+    val = _queue[_q_btm];
+    _q_btm = (_q_btm + 1) % QUEUESIZE;
   }
   return val;	
 }
 
 // 取出し可能チェック
 uint8_t ICACHE_RAM_ATTR TPS2::available() {
-  uint8_t d = _q_top != _q_btm ? 1: 0; 
+  uint8_t d = _q_top != _q_btm ? 1 : 0;
   return d;
 }
