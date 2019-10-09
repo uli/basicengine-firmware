1dc601446cc12b8f300e72d4626db820627c29a5
Ulrich Hecht <uli@basicengine.org>
2019-10-09 15:34:13 +0200

    tGraphicDev: format code
    No functional changes.

--- libraries/TTBAS_LIB/tGraphicDev.cpp
+++ libraries/TTBAS_LIB/tGraphicDev.cpp
@@ -10,49 +10,53 @@
 
 // 初期化
 void tGraphicDev::init() {
-  gwidth   = tv_get_gwidth();
-  gheight  = tv_get_gheight();  
+  gwidth = tv_get_gwidth();
+  gheight = tv_get_gheight();
 }
 
 // グラフックスクリーン横幅取得
 uint16_t GROUP(basic_video) tGraphicDev::getGWidth() {
-	return gwidth;
+  return gwidth;
 }
 
 // グラフックスクリーン縦幅取得
 uint16_t GROUP(basic_video) tGraphicDev::getGHeight() {
-	return gheight;
-} 
+  return gheight;
+}
 
 // ドット描画
 void GROUP(basic_video) tGraphicDev::pset(int16_t x, int16_t y, pixel_t c) {
-  tv_pset(x,y,c);
+  tv_pset(x, y, c);
 }
 
 // 線の描画
-void GROUP(basic_video) tGraphicDev::line(int16_t x1, int16_t y1, int16_t x2, int16_t y2, pixel_t c) {
-  tv_line(x1,y1,x2,y2,c);
+void GROUP(basic_video) tGraphicDev::line(int16_t x1, int16_t y1, int16_t x2,
+                                          int16_t y2, pixel_t c) {
+  tv_line(x1, y1, x2, y2, c);
 }
 
 // 円の描画
-void GROUP(basic_video) tGraphicDev::circle(int16_t x, int16_t y, int16_t r, pixel_t c, int f) {
+void GROUP(basic_video) tGraphicDev::circle(int16_t x, int16_t y, int16_t r,
+                                            pixel_t c, int f) {
   tv_circle(x, y, r, c, f);
 }
 
 // 四角の描画
-void GROUP(basic_video) tGraphicDev::rect(int16_t x, int16_t y, int16_t w, int16_t h, pixel_t c, int f) {
+void GROUP(basic_video) tGraphicDev::rect(int16_t x, int16_t y, int16_t w,
+                                          int16_t h, pixel_t c, int f) {
   tv_rect(x, y, w, h, c, f);
 }
 
 // グラフィックスクロール
-void GROUP(basic_video) tGraphicDev::gscroll(int16_t x, int16_t y, int16_t w, int16_t h, uint8_t mode) {
+void GROUP(basic_video) tGraphicDev::gscroll(int16_t x, int16_t y, int16_t w,
+                                             int16_t h, uint8_t mode) {
   tv_gscroll(x, y, w, h, mode);
 }
 
 void GROUP(basic_video) tGraphicDev::set_gcursor(uint16_t x, uint16_t y) {
-  tv_set_gcursor(x,y);
+  tv_set_gcursor(x, y);
 }
 
 void GROUP(basic_video) tGraphicDev::gputch(uint8_t c) {
- tv_write(c); 
+  tv_write(c);
 }
--- libraries/TTBAS_LIB/tGraphicDev.h
+++ libraries/TTBAS_LIB/tGraphicDev.h
@@ -11,29 +11,32 @@
 
 #include <Arduino.h>
 
-const uint8_t* tv_getFontAdr() ;
+const uint8_t *tv_getFontAdr();
 
 class tGraphicDev {
-  protected:
-    uint16_t gwidth;             // Screen graphic horizontal size
-    uint16_t gheight;            // Screen graphic vertical size
+protected:
+  uint16_t gwidth;   // Screen graphic horizontal size
+  uint16_t gheight;  // Screen graphic vertical size
 
-  public:
-     void  init();
-    inline const uint8_t *getfontadr() { return tv_getFontAdr()+3; };   // Font address pointer
-    virtual uint16_t getGWidth();       // Graphics screen width acquisition
-    virtual uint16_t getGHeight();      // Graphics screen vertical width acquisition
+public:
+  void init();
+  inline const uint8_t *getfontadr() {
+    return tv_getFontAdr() + 3;
+  };                              // Font address pointer
+  virtual uint16_t getGWidth();   // Graphics screen width acquisition
+  virtual uint16_t getGHeight();  // Graphics screen vertical width acquisition
 
-    // Graphic drawing
-    virtual void pset(int16_t x, int16_t y, pixel_t c);
-    virtual void line(int16_t x1, int16_t y1, int16_t x2, int16_t y2, pixel_t c);
-    virtual void circle(int16_t x, int16_t y, int16_t r, pixel_t c, int f);
-    virtual void rect(int16_t x, int16_t y, int16_t w, int16_t h, pixel_t c, int f);
-    //virtual void cscroll(int16_t x, int16_t y, int16_t w, int16_t h, uint8_t d);
-    virtual void gscroll(int16_t x, int16_t y, int16_t w, int16_t h, uint8_t mode);
-    virtual void set_gcursor(uint16_t, uint16_t);
-    virtual void gputch(uint8_t c);
-
+  // Graphic drawing
+  virtual void pset(int16_t x, int16_t y, pixel_t c);
+  virtual void line(int16_t x1, int16_t y1, int16_t x2, int16_t y2, pixel_t c);
+  virtual void circle(int16_t x, int16_t y, int16_t r, pixel_t c, int f);
+  virtual void rect(int16_t x, int16_t y, int16_t w, int16_t h, pixel_t c,
+                    int f);
+  //virtual void cscroll(int16_t x, int16_t y, int16_t w, int16_t h, uint8_t d);
+  virtual void gscroll(int16_t x, int16_t y, int16_t w, int16_t h,
+                       uint8_t mode);
+  virtual void set_gcursor(uint16_t, uint16_t);
+  virtual void gputch(uint8_t c);
 };
 
 #endif
