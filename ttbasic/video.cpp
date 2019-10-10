be24b8ec4b0777a26b8343e99f48c91a20d8e159
Ulrich Hecht <uli@basicengine.org>
2019-10-10 12:42:01 +0200

    video: format touch-ups
    No functional changes.

--- ttbasic/video.cpp
+++ ttbasic/video.cpp
@@ -4,27 +4,28 @@
 #include "video_driver.h"
 #include "colorspace.h"
 
-bool Video::allocBacking(int w, int h, int &x, int &y)
-{
-  Rect r = m_bin.Insert(w, h, false, GuillotineBinPack::RectBestAreaFit, GuillotineBinPack::Split256);
-  x = r.x; y = r.y + m_current_mode.y;
+bool Video::allocBacking(int w, int h, int &x, int &y) {
+  Rect r = m_bin.Insert(w, h, false, GuillotineBinPack::RectBestAreaFit,
+                        GuillotineBinPack::Split256);
+  x = r.x;
+  y = r.y + m_current_mode.y;
   return r.height != 0;
 }
 
-void Video::freeBacking(int x, int y, int w, int h)
-{
+void Video::freeBacking(int x, int y, int w, int h) {
   Rect r;
-  r.x = x; r.y = y - m_current_mode.y; r.width = w; r.height = h;
+  r.x = x;
+  r.y = y - m_current_mode.y;
+  r.width = w;
+  r.height = h;
   m_bin.Free(r, true);
 }
 
-void Video::reset()
-{
+void Video::reset() {
   m_bin.Init(m_current_mode.x, m_last_line - m_current_mode.y);
 }
 
-void Video::setColorSpace(uint8_t palette)
-{
+void Video::setColorSpace(uint8_t palette) {
   switch (palette) {
   case 0: csp.setColorConversion(0, 7, 3, 6, true); break;
   case 1: csp.setColorConversion(1, 7, 4, 7, true); break;
