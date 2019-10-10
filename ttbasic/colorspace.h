eea1ccee59739eeb9cd7c68cce925f58b404a79c
Ulrich Hecht <uli@basicengine.org>
2019-10-10 11:58:54 +0200

    colorspace: colorspace.h: format code
    No functional changes.

--- ttbasic/colorspace.h
+++ ttbasic/colorspace.h
@@ -7,35 +7,36 @@
 #include "video_driver.h"
 #include <stdint.h>
 
-#define CSP_NUM_COLORSPACES	2
+#define CSP_NUM_COLORSPACES 2
 
 class Colorspace {
 public:
-    pixel_t colorFromRgb(uint8_t r, uint8_t g, uint8_t b);
-    inline pixel_t colorFromRgb(uint8_t *c) {
-      return colorFromRgb(c[0], c[1], c[2]);
-    }
+  pixel_t colorFromRgb(uint8_t r, uint8_t g, uint8_t b);
+  inline pixel_t colorFromRgb(uint8_t *c) {
+    return colorFromRgb(c[0], c[1], c[2]);
+  }
 
-    static void setColorConversion(int yuvpal, int h_weight, int s_weight, int v_weight, bool fixup);
-    ipixel_t indexedColorFromRgb(uint8_t r, uint8_t g, uint8_t b);
-    inline ipixel_t indexedColorFromRgb(uint8_t *c) {
-      return indexedColorFromRgb(c[0], c[1], c[2]);
-    }
+  static void setColorConversion(int yuvpal, int h_weight, int s_weight,
+                                 int v_weight, bool fixup);
+  ipixel_t indexedColorFromRgb(uint8_t r, uint8_t g, uint8_t b);
+  inline ipixel_t indexedColorFromRgb(uint8_t *c) {
+    return indexedColorFromRgb(c[0], c[1], c[2]);
+  }
 
-    inline void setColorSpace(uint8_t palette) {
-      m_colorspace = palette;
-    }
-    inline uint8_t getColorSpace() {
-      return m_colorspace;
-    }
-    uint8_t *paletteData(uint8_t colorspace);
+  inline void setColorSpace(uint8_t palette) {
+    m_colorspace = palette;
+  }
+  inline uint8_t getColorSpace() {
+    return m_colorspace;
+  }
+  uint8_t *paletteData(uint8_t colorspace);
 
-    pixel_t fromIndexed(ipixel_t c);
+  pixel_t fromIndexed(ipixel_t c);
 
 private:
-    ipixel_t indexedColorFromRgbSlow(uint8_t r, uint8_t g, uint8_t b);
+  ipixel_t indexedColorFromRgbSlow(uint8_t r, uint8_t g, uint8_t b);
 
-    uint8_t m_colorspace;
+  uint8_t m_colorspace;
 };
 
 extern Colorspace csp;
