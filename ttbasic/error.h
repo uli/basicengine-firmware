8d72c12ece2583d9567c0b3274cc9f44bd380fde
Ulrich Hecht <uli@basicengine.org>
2019-10-10 12:02:44 +0200

    error.h: format touch-ups
    No functional changes.

--- ttbasic/error.h
+++ ttbasic/error.h
@@ -3,7 +3,7 @@
 
 extern uint8_t err;
 
-#define ESTR(n,s) n,
+#define ESTR(n, s) n,
 
 // エラーコード
 enum {
@@ -12,10 +12,10 @@ enum {
 
 #undef ESTR
 
-#define SD_ERR_INIT		ERR_SD_NOT_READY
-#define SD_ERR_OPEN_FILE	ERR_FILE_OPEN
-#define SD_ERR_NOT_FILE		ERR_BAD_FNAME
-#define SD_ERR_READ_FILE	ERR_FILE_READ
-#define SD_ERR_WRITE_FILE	ERR_FILE_WRITE
+#define SD_ERR_INIT       ERR_SD_NOT_READY
+#define SD_ERR_OPEN_FILE  ERR_FILE_OPEN
+#define SD_ERR_NOT_FILE   ERR_BAD_FNAME
+#define SD_ERR_READ_FILE  ERR_FILE_READ
+#define SD_ERR_WRITE_FILE ERR_FILE_WRITE
 
 #endif
