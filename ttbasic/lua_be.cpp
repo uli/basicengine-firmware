d106a79edb922a536781817ee53612cfb0816973
Ulrich Hecht <uli@basicengine.org>
2019-10-10 12:19:59 +0200

    ttbasic: lua: format code
    No functional changes.

--- ttbasic/lua_be.cpp
+++ ttbasic/lua_be.cpp
@@ -4,21 +4,18 @@
 #include "basic.h"
 #include "lua_defs.h"
 
-static int l_cls(lua_State *l)
-{
+static int l_cls(lua_State *l) {
   sc0.cls();
-  sc0.locate(0,0);
+  sc0.locate(0, 0);
   return 0;
 }
 
-static int l_basic(lua_State *l)
-{
+static int l_basic(lua_State *l) {
   luaL_error(l, "return to basic");
   return 0;
 }
 
-int luaopen_be(lua_State *l)
-{
+int luaopen_be(lua_State *l) {
   lua_pushcfunction(l, l_cls);
   lua_setglobal_P(l, "cls");
   lua_pushcfunction(l, l_basic);
--- ttbasic/lua_video.cpp
+++ ttbasic/lua_video.cpp
@@ -4,27 +4,25 @@
 #include "basic.h"
 #include "lua_defs.h"
 
-static int l_locate(lua_State *l)
-{
-    int32_t x = luaL_checknumber(l, -1);
-    int32_t y = luaL_checknumber(l, -2);
-    if (x >= sc0.getWidth())
-        x = sc0.getWidth() - 1;
-    else if (x < 0)
-        x = 0;
-    if (y >= sc0.getHeight())
-        y = sc0.getHeight() - 1;
-    else if (y < 0)
-        y = 0;
+static int l_locate(lua_State *l) {
+  int32_t x = luaL_checknumber(l, -1);
+  int32_t y = luaL_checknumber(l, -2);
+  if (x >= sc0.getWidth())
+    x = sc0.getWidth() - 1;
+  else if (x < 0)
+    x = 0;
+  if (y >= sc0.getHeight())
+    y = sc0.getHeight() - 1;
+  else if (y < 0)
+    y = 0;
 
-    sc0.locate((uint16_t)x, (uint16_t)y);
+  sc0.locate((uint16_t)x, (uint16_t)y);
 
-    return 0;
+  return 0;
 }
 
-int luaopen_video(lua_State *l)
-{
-    lua_pushcfunction(l, l_locate);
-    lua_setglobal_P(l, "locate");
-    return 0;
+int luaopen_video(lua_State *l) {
+  lua_pushcfunction(l, l_locate);
+  lua_setglobal_P(l, "locate");
+  return 0;
 }
