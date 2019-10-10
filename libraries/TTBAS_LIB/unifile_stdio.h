a2742d6b28242f7dfc906d42c80cb8dcb367c096
Ulrich Hecht <uli@basicengine.org>
2019-10-10 11:20:52 +0200

    Unifile: unifile_stdio.h: indentation fixes
    No functional changes.

--- libraries/TTBAS_LIB/unifile_stdio.h
+++ libraries/TTBAS_LIB/unifile_stdio.h
@@ -30,12 +30,12 @@ int _ferror(FILE *stream);
 void _clearerr(FILE *stream);
 
 struct dirent {
-	unsigned char d_type;
-	char d_name[64];       /* filename */
+  unsigned char d_type;
+  char d_name[64]; /* filename */
 };
 
-#define DT_DIR	4
-#define DT_REG	8
+#define DT_DIR 4
+#define DT_REG 8
 
 DIR *_opendir(const char *name);
 int _closedir(DIR *dir);
@@ -48,12 +48,12 @@ int _chdir(const char *path);
 char *_getcwd(char *buf, size_t size);
 
 struct stat {
-	mode_t st_mode;
-	off_t st_size;
+  mode_t st_mode;
+  off_t st_size;
 };
 
-#define S_IFDIR	0040000
-#define S_IFREG	0100000
+#define S_IFDIR 0040000
+#define S_IFREG 0100000
 
 int _stat(const char *pathname, struct stat *buf);
 
@@ -64,19 +64,19 @@ int _rmdir(const char *pathname);
 }
 #endif
 
-#define fopen _fopen
+#define fopen  _fopen
 #define fclose _fclose
-#define fread _fread
+#define fread  _fread
 #define fwrite _fwrite
-#define fputs _fputs
-#define fseek _fseek
-#define ftell _ftell
-#define feof _feof
+#define fputs  _fputs
+#define fseek  _fseek
+#define ftell  _ftell
+#define feof   _feof
 #undef getc
-#define getc _getc
+#define getc   _getc
 #define ungetc _ungetc
 #undef putc
-#define putc _putc
-#define fflush _fflush
-#define ferror _ferror
+#define putc     _putc
+#define fflush   _fflush
+#define ferror   _ferror
 #define clearerr _clearerr
