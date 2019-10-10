c017158c68a9248f90625aebf7ab8fdd54b97f6f
Ulrich Hecht <uli@basicengine.org>
2019-10-10 11:30:07 +0200

    sdl: scalers.h: format code
    No functional changes.

--- sdl/scalers.h
+++ sdl/scalers.h
@@ -5,14 +5,14 @@
 
 #include <SDL/SDL.h>
 
-#define SCALER(x) scaler_ ## x ## _32
-#define SCALER_TYPE scaler_32_t
-#define SCALERS_UP upscalers_32
-#define SCALERS_DOWN downscalers_32
-#define SCALE_FUNC scale_integral_32
+#define SCALER(x)       scaler_##x##_32
+#define SCALER_TYPE     scaler_32_t
+#define SCALERS_UP      upscalers_32
+#define SCALERS_DOWN    downscalers_32
+#define SCALE_FUNC      scale_integral_32
 #define SCALER_SRC_TYPE uint32_t
 #define SCALER_DST_TYPE uint32_t
-#define CONV(p) (p)
+#define CONV(p)         (p)
 
 #include "scalers_imp.h"
 
@@ -25,14 +25,14 @@
 #undef SCALER_DST_TYPE
 #undef CONV
 
-#define SCALER(x) scaler_ ## x ## _16
-#define SCALER_TYPE scaler_16_t
-#define SCALERS_UP upscalers_16
-#define SCALERS_DOWN downscalers_16
-#define SCALE_FUNC scale_integral_16
+#define SCALER(x)       scaler_##x##_16
+#define SCALER_TYPE     scaler_16_t
+#define SCALERS_UP      upscalers_16
+#define SCALERS_DOWN    downscalers_16
+#define SCALE_FUNC      scale_integral_16
 #define SCALER_SRC_TYPE uint16_t
 #define SCALER_DST_TYPE uint16_t
-#define CONV(p) (p)
+#define CONV(p)         (p)
 
 #include "scalers_imp.h"
 
@@ -45,14 +45,14 @@
 #undef SCALER_DST_TYPE
 #undef CONV
 
-#define SCALER(x) scaler_ ## x ## _8
-#define SCALER_TYPE scaler_8_t
-#define SCALERS_UP upscalers_8
-#define SCALERS_DOWN downscalers_8
-#define SCALE_FUNC scale_integral_8
+#define SCALER(x)       scaler_##x##_8
+#define SCALER_TYPE     scaler_8_t
+#define SCALERS_UP      upscalers_8
+#define SCALERS_DOWN    downscalers_8
+#define SCALE_FUNC      scale_integral_8
 #define SCALER_SRC_TYPE uint8_t
 #define SCALER_DST_TYPE uint8_t
-#define CONV(p) (p)
+#define CONV(p)         (p)
 
 #include "scalers_imp.h"
 
@@ -64,4 +64,3 @@
 #undef SCALER_SRC_TYPE
 #undef SCALER_DST_TYPE
 #undef CONV
-
