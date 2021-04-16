c69e8a7247cadb7299f7b44fd61055430b50a244
Ulrich Hecht <uli@basicengine.org>
2021-04-16 16:03:13 +0200

    sdlgfx: blitRect(): replace SDL_BlitSurface() with H3 implementation
    SDL_BlitSurface() craps up when there is an alpha channel on the text
    surface. Might be some optimization that fails when the source and
    destination are overlapping?
    
    XXX: Unify with H3 implementation.

--- sdl/sdlgfx.cpp
+++ sdl/sdlgfx.cpp
@@ -77,10 +77,41 @@ void SDLGFX::reset() {
 
 void SDLGFX::blitRect(uint16_t x_src, uint16_t y_src, uint16_t x_dst,
                       uint16_t y_dst, uint16_t width, uint16_t height) {
-  SDL_Rect src = { (Sint16)x_src, (Sint16)y_src, width, height };
-  SDL_Rect dst = { (Sint16)x_dst, (Sint16)y_dst, width, height };
-  SDL_BlitSurface(m_text_surface, &src, m_text_surface, &dst);
-  m_dirty = true;
+#ifdef PROFILE_BLIT
+  uint32_t s = micros();
+#endif
+  if (y_dst == y_src && x_dst > x_src) {
+    while (height) {
+      memmove(&PIXELT(x_dst, y_dst), &PIXELT(x_src, y_src),
+              width * sizeof(pixel_t));
+      y_dst++;
+      y_src++;
+      height--;
+    }
+  } else if ((y_dst > y_src) || (y_dst == y_src && x_dst > x_src)) {
+    y_dst += height - 1;
+    y_src += height - 1;
+    while (height) {
+      memcpy(&PIXELT(x_dst, y_dst), &PIXELT(x_src, y_src),
+             width * sizeof(pixel_t));
+      y_dst--;
+      y_src--;
+      height--;
+    }
+  } else {
+    while (height) {
+      memcpy(&PIXELT(x_dst, y_dst), &PIXELT(x_src, y_src),
+             width * sizeof(pixel_t));
+      y_dst++;
+      y_src++;
+      height--;
+    }
+  }
+#ifdef PROFILE_BLIT
+  uint32_t e = micros();
+  if (e - s > 1000)
+    printf("blit %d us\n", e - s);
+#endif
 }
 
 void SDLGFX::fillRect(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2,
