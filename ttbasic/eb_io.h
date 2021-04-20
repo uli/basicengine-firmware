9dae42724f50b0c94b8e7237a528eabc438797f3
Ulrich Hecht <uli@basicengine.org>
2021-04-20 12:10:58 +0200

    eb_io.h: add missing extern "C"

--- ttbasic/eb_io.h
+++ ttbasic/eb_io.h
@@ -1,5 +1,13 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int eb_gpio_set_pin(int portno, int pinno, int data);
 int eb_gpio_get_pin(int portno, int pinno);
 int eb_gpio_set_pin_mode(int portno, int pinno, int mode);
 int eb_i2c_write(unsigned char addr, const char *data, int count);
 int eb_i2c_read(unsigned char addr, char *data, int count);
+
+#ifdef __cplusplus
+}
+#endif
