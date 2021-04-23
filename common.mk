ce54e55790fa65d83b19bcf788577cc6149e0eec
Ulrich Hecht <uli@basicengine.org>
2021-04-23 17:41:06 +0200

    Makefile.sdl, Makefile.h3: lots of intertwined changes
    - move shared bits to common.mk
    - build SDL with dyncall and tcc
    - build H3 with -O3
    - disabled useless warnings on SDL as well
    - move SDL objects to build_sdl
    - probably something I forgot

---
+++ common.mk
@@ -0,0 +1,51 @@
+COMMON_ARDFLAGS = -DENGINEBASIC -O3 -g -Iarduino_compat -Ittbasic $(INCLUDE) \
+	   -Wall -Wno-unused -Wno-sign-compare -Wno-implicit-fallthrough \
+	   -D_GNU_SOURCE
+
+COMMON_SOURCES = $(shell ls libraries/lua/*.cpp) $(shell ls ttbasic/*.cpp) $(shell ls libraries/TTBAS_LIB/*.cpp) \
+	$(shell ls libraries/ESP8266SAM/src/*.cpp) $(shell ls libraries/TTVoutfonts/*.cpp) \
+	$(shell ls libraries/azip/*.cpp) \
+	$(shell ls libraries/alpha-lib/src/*.cpp) \
+	$(shell ls arduino_compat/*.cpp) \
+
+COMMON_SOURCES_C = $(shell ls ttbasic/*.c) $(shell ls libraries/stb/*.c) \
+	libraries/tinycc/libtcc.c
+
+PROF = $(SOURCES:.cpp=.gcda) $(SOURCES:.cpp=.gcno) \
+	$(SOURCES_C:.c=.gcda) $(SOURCES_C:.c=.gcno)
+
+COMMON_INCLUDE = -Ilibraries/TTVoutfonts -Ilibraries/TTBAS_LIB -Ilibraries/TKeyboard/src \
+	-Ilibraries/ESP8266SAM/src -Ilibraries/azip -Ilibraries/lua -Ilibraries/stb \
+	-Ilibraries/alpha-lib/include -Igfx -Ilibraries/dyncall/dyncall
+
+$(OBJDIR)/%.o: %.c
+	@mkdir -p $(@D)
+	$(CC) $(CFLAGS) -o $@ -c $<
+
+$(OBJDIR)/%.o: %.cpp
+	@mkdir -p $(@D)
+	$(CXX) $(CXXFLAGS) -o $@ -c $<
+
+$(OBJDIR)/%.o: %.S
+	@mkdir -p $(@D)
+	$(CC) $(CFLAGS) -o $@ -c $<
+
+$(OBJDIR)/ttbasic/basic_help.o: $(OBJDIR)/ttbasic/helptext.o
+ttbasic/helptext.cpp: ttbasic/scripts/help.py $(shell ls ttbasic/basic*.cpp)
+	$(MAKE) -C ttbasic -f autogen.mk helptext.cpp
+
+ttbasic/funtbl.h: ttbasic/icode.txt
+	$(MAKE) -C ttbasic -f autogen.mk funtbl.h epigrams.h version.h
+$(OUT_OBJS): ttbasic/funtbl.h
+$(SOURCES): ttbasic/funtbl.h
+
+$(OBJDIR)/dyncall/Makefile.config:
+	mkdir -p $(OBJDIR)/dyncall
+	cd $(OBJDIR)/dyncall ; CC=$(CC) CXX=$(CXX) ../../libraries/dyncall/configure
+
+$(OBJDIR)/dyncall/dyncall/libdyncall_s.a: $(OBJDIR)/dyncall/Makefile.config
+	CC=$(CC) CXX=$(CXX) $(MAKE) -C $(OBJDIR)/dyncall/dyncall
+
+gcov:
+	rm -f *.gcov
+	gcov $(SOURCES) $(SOURCES_C)
