655eba963484b7141bb5e16961e6ad3b57a15f81
Ulrich Hecht <uli@basicengine.org>
2021-04-30 14:17:54 +0200

    basic_native: init_tcc(): set HOME to something sensible

--- ttbasic/basic_native.cpp
+++ ttbasic/basic_native.cpp
@@ -61,6 +61,12 @@ void Basic::init_tcc() {
   // add empty module to make default symbols visible to BASIC
   struct module system = { new_tcc(), "system", NULL, 0 };
   modules.push_back(system);
+
+#ifdef SDL
+  setenv("HOME", getenv("ENGINEBASIC_ROOT"), 1);
+#else
+  setenv("HOME", "/", 1);
+#endif
 }
 
 static TCCState *current_tcc;
