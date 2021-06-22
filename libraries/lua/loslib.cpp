e9827214e47b9bc7161ac0eec14af2bc51e2a750
Ulrich Hecht <uli@basicengine.org>
2021-06-22 14:40:47 +0200

    lua: loslib: use standard remove(), rename() functions

--- libraries/lua/loslib.cpp
+++ libraries/lua/loslib.cpp
@@ -151,14 +151,14 @@ static int os_execute (lua_State *L) {
 
 static int os_remove (lua_State *L) {
   const char *filename = luaL_checkstring(L, 1);
-  return luaL_fileresult(L, _remove(filename) == 0, filename);
+  return luaL_fileresult(L, remove(filename) == 0, filename);
 }
 
 
 static int os_rename (lua_State *L) {
   const char *fromname = luaL_checkstring(L, 1);
   const char *toname = luaL_checkstring(L, 2);
-  return luaL_fileresult(L, _rename(fromname, toname) == 0, NULL);
+  return luaL_fileresult(L, rename(fromname, toname) == 0, NULL);
 }
 
 
