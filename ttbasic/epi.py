35b3cb618b6a37183bf921c56cc799c1a8dd5bca
Ulrich Hecht <uli@basicengine.org>
2021-10-21 11:27:31 +0200

    epi.py: make Python 3-compatible
    Apparently Python 3 is now the default in some distributions.
    
    Fixes issue #79.

--- ttbasic/epi.py
+++ ttbasic/epi.py
@@ -1,9 +1,10 @@
+from __future__ import print_function
 import sys
 count=0
 for i in sys.stdin.readlines():
-  print 'static const char __ep%d [] PROGMEM = "%s";' % (count, i.strip())
+  print('static const char __ep%d [] PROGMEM = "%s";' % (count, i.strip()))
   count += 1
-print 'static PROGMEM const char * const epigrams[] = {'
+print('static PROGMEM const char * const epigrams[] = {')
 for i in range(0, count):
-  print '__ep%d, ' % i,
-print '\n};'
+  print('__ep%d, ' % i,)
+print('\n};')
