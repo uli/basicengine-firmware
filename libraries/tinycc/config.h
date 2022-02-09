3ab341fcf9c9ce70be5b96313817918be2853bae
Ulrich Hecht <uli@basicengine.org>
2022-02-09 16:37:49 +0100

    dump tinycc, replaced with external program

--- libraries/tinycc/config.h
+++
@@ -1,8 +0,0 @@
-#define CONFIG_TCC_STATIC
-#ifdef __arm__
-#define TCC_ARM_VFP
-#endif
-#define TCC_VERSION "0.9.27"
-#define TCC_IS_NATIVE
-#define CONFIG_TCCBOOT
-#define CONFIG_TCC_SYSINCLUDEPATHS ""
