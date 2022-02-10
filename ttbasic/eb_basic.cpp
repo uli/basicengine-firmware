9f527e8c361c8a3246741e93542c1693f114a102
Ulrich Hecht <uli@basicengine.org>
2022-02-10 11:58:03 +0100

    eb_basic, eb_bg, eb_config, eb_conio: make APIs visible

--- ttbasic/eb_basic.cpp
+++ ttbasic/eb_basic.cpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2021 Ulrich Hecht
 
+#include "eb_api.h"
 #include "eb_basic.h"
 #include "eb_basic_int.h"
 #include "basic.h"
@@ -28,7 +29,7 @@ std::vector<struct eb_command_t> custom_commands;
 std::vector<struct eb_numfun_t> custom_numfuns;
 std::vector<struct eb_strfun_t> custom_strfuns;
 
-int eb_add_command(const char *name, const token_t *syntax, eb_command_handler_t handler) {
+EBAPI int eb_add_command(const char *name, const token_t *syntax, eb_command_handler_t handler) {
     eb_command_t cmd = { (token_t)kwtbl.size(), syntax, handler };
     custom_commands.push_back(cmd);
     kwtbl.push_back(strdup(name));
@@ -40,7 +41,7 @@ int eb_add_command(const char *name, const token_t *syntax, eb_command_handler_t
     return 0;
 }
 
-int eb_add_numfun(const char *name, const token_t *syntax, eb_numfun_handler_t handler) {
+EBAPI int eb_add_numfun(const char *name, const token_t *syntax, eb_numfun_handler_t handler) {
     eb_numfun_t nf = { (token_t)kwtbl.size(), syntax, handler };
     custom_numfuns.push_back(nf);
     kwtbl.push_back(strdup(name));
@@ -52,7 +53,7 @@ int eb_add_numfun(const char *name, const token_t *syntax, eb_numfun_handler_t h
     return 0;
 }
 
-int eb_add_strfun(const char *name, const token_t *syntax, eb_strfun_handler_t handler) {
+EBAPI int eb_add_strfun(const char *name, const token_t *syntax, eb_strfun_handler_t handler) {
     eb_strfun_t sf = { (token_t)kwtbl.size(), syntax, handler };
     custom_strfuns.push_back(sf);
     kwtbl.push_back(strdup(name));
@@ -64,7 +65,7 @@ int eb_add_strfun(const char *name, const token_t *syntax, eb_strfun_handler_t h
     return 0;
 }
 
-const token_t *eb_command_syntax(token_t token) {
+EBAPI const token_t *eb_command_syntax(token_t token) {
     for (auto cmd : custom_commands) {
         if (cmd.token == token)
             return cmd.syntax;
@@ -72,7 +73,7 @@ const token_t *eb_command_syntax(token_t token) {
     return NULL;
 }
 
-const token_t *eb_numfun_syntax(token_t token) {
+EBAPI const token_t *eb_numfun_syntax(token_t token) {
     for (auto nf : custom_numfuns) {
         if (nf.token == token)
             return nf.syntax;
@@ -80,7 +81,7 @@ const token_t *eb_numfun_syntax(token_t token) {
     return NULL;
 }
 
-const token_t *eb_strfun_syntax(token_t token) {
+EBAPI const token_t *eb_strfun_syntax(token_t token) {
     for (auto sf : custom_strfuns) {
         if (sf.token == token)
             return sf.syntax;
@@ -118,31 +119,31 @@ BString Basic::handle_strfun(enum token_t token, const eb_param_t *params) {
     return BString();
 }
 
-const char **eb_kwtbl(void) {
+EBAPI const char **eb_kwtbl(void) {
     return &kwtbl[0];
 }
 
-int eb_kwtbl_size(void) {
+EBAPI int eb_kwtbl_size(void) {
     return kwtbl.size();
 }
 
-void *eb_new_basic_context(void) {
+EBAPI void *eb_new_basic_context(void) {
     return (void *) new Basic;
 }
 
-void eb_delete_basic_context(void *bc) {
+EBAPI void eb_delete_basic_context(void *bc) {
     delete (Basic *)bc;
 }
 
-int eb_exec_basic(void *bc, const char *filename) {
+EBAPI int eb_exec_basic(void *bc, const char *filename) {
     return ((Basic *)bc)->exec(filename);
 }
 
-void eb_set_error(int error, const char *expected) {
+EBAPI void eb_set_error(int error, const char *expected) {
     err = error;
     err_expected = expected;
 }
 
-int eb_get_error(void) {
+EBAPI int eb_get_error(void) {
     return err;
 }
--- ttbasic/eb_config.cpp
+++ ttbasic/eb_config.cpp
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2021 Ulrich Hecht
 
+#include "eb_api.h"
 #include "eb_config.h"
 #include "basic.h"
 
-unsigned int eb_theme_color(int index) {
+EBAPI unsigned int eb_theme_color(int index) {
     return csp.colorFromRgb(CONFIG.color_scheme[index]);
 }
