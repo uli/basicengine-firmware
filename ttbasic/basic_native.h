2de5ac9d9e1354edc0a3cbeed813837ed1f49100
Ulrich Hecht <uli@basicengine.org>
2023-05-17 11:40:40 +0200

    basic_sys: break shell_list() out of SHELL command

--- ttbasic/basic_native.h
+++ ttbasic/basic_native.h
@@ -23,4 +23,6 @@ extern std::vector<struct module> modules;
 
 #include <list>
 int exec_list(std::list<BString> &args);
+int run_list(std::list<BString> &args);
+void shell_list(std::list<BString> &args);
 #endif
