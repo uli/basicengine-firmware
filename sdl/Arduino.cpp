288c055b6b3039351c780362f9c73015b0a01f02
Ulrich Hecht <uli@basicengine.org>
2024-08-20 13:05:58 +0200

    sdl: android: Arduino.cpp: redirect stdio to Android logs

--- sdl/Arduino.cpp
+++ sdl/Arduino.cpp
@@ -25,6 +25,52 @@ static void my_exit(void) {
   vs23.end();
 }
 
+#ifdef ANDROID
+
+// Redirect stdout/stderr to the Android logging facilities.
+// Adapted from
+// https://stackoverflow.com/questions/10531050/redirect-stdout-to-logcat-in-android-ndk/42715692#42715692
+
+#include <android/log.h>
+#include <pthread.h>
+
+static int pfd[2];
+static pthread_t thr;
+static const char *tag;
+
+static void *thread_func(void*)
+{
+    ssize_t rdsz;
+    char buf[128];
+    while((rdsz = read(pfd[0], buf, sizeof buf - 1)) > 0) {
+        if(buf[rdsz - 1] == '\n') --rdsz;
+        buf[rdsz] = 0;  /* add null-terminator */
+        __android_log_write(ANDROID_LOG_DEBUG, tag, buf);
+    }
+    return 0;
+}
+
+int start_logger(const char *app_name)
+{
+    tag = app_name;
+
+    /* make stdout line-buffered and stderr unbuffered */
+    setvbuf(stdout, 0, _IOLBF, 0);
+    setvbuf(stderr, 0, _IONBF, 0);
+
+    /* create the pipe and redirect stdout and stderr */
+    pipe(pfd);
+    dup2(pfd[1], 1);
+    dup2(pfd[1], 2);
+
+    /* spawn the logging thread */
+    if(pthread_create(&thr, 0, thread_func, 0) == -1)
+        return -1;
+    pthread_detach(thr);
+    return 0;
+}
+#endif
+
 #include <SDL.h>
 
 extern "C" void init_idle();
@@ -36,6 +82,10 @@ SDL_Renderer *sdl_renderer;
 
 int main(int argc, char **argv) {
   int opt;
+
+#ifdef ANDROID
+  start_logger("enginebasic");
+#endif
 
   char *path = getcwd(NULL, 0);
   if (path) {
